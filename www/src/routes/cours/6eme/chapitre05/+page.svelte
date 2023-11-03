f647e94898c34be68a3e11d65b126aed126a9329
Valentin Mastro <valentinmastro@hotmail.fr>
2023-11-03 16:21:36 +0100

    chapitre 05 de 6eme terminé

--- www/src/routes/cours/6eme/chapitre05/+page.svelte
+++ www/src/routes/cours/6eme/chapitre05/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import Chapitre from "$lib/chapitrage/Chapitre.svelte";
 	import Partie from "$lib/chapitrage/Partie.svelte";
+	import SousPartie from "$lib/chapitrage/SousPartie.svelte";
 	import Definition from "$lib/chapitrage/sections/Definition.svelte";
 	import Exemples from "$lib/chapitrage/sections/Exemples.svelte";
+	import Paragraphe from "$lib/chapitrage/sections/Paragraphe.svelte";
+	import Propriete from "$lib/chapitrage/sections/Propriete.svelte";
 	import Remarque from "$lib/chapitrage/sections/Remarque.svelte";
 	import Katex from "svelte-katex";
 
@@ -37,9 +40,90 @@
         <Remarque>
             Le dénominateur <Katex>b \neq 0</Katex>. <br> (on ne peut pas diviser par 0)
         </Remarque>
-        <div style='height:20vw'>
-
-        </div>
+    </Partie>
+    <Partie numero={2} titre="Calculer la fraction d'un nombre">
+        <Paragraphe couleur="red" nom="Règle">
+            Pour multipler une fraction avec un nombre, on multiplie le nombre avec le numérateur et on divise par le dénominateur.
+        </Paragraphe>
+        <Exemples>
+            <ul>
+                <li>
+                    <Katex>
+                        {"\\dfrac{3}{4} \\text{ de } 36 = \\dfrac{3}{4} \\times 36 = 3 \\times 36 \\div 4"}
+                    </Katex>
+                </li>
+                <li>
+                    <Katex>
+                        {"\\dfrac{7}{9} \\text{ de } 81 = \\dfrac{7}{9} \\times 81 = 7 \\times 81 \\div 9"}
+                    </Katex>
+                </li>
+            </ul>
+        </Exemples>
+    </Partie>
+    <Partie numero={3} titre="Calculs">
+        <SousPartie numero={1} titre="Additionner des fractions">
+            <Paragraphe couleur="red" nom="Règle">
+                Pour additionner deux fractions :
+                <ul>
+                    <li>il faut que les dénominateurs soient égaux</li>
+                    <li>on additionne les numérateurs</li>
+                </ul>
+            </Paragraphe>
+            <Exemples>
+                <ul>
+                    <li>
+                        <Katex>
+                            {"\\dfrac{8}{9} + \\dfrac{4}{9} = \\dfrac{12}{9}"}
+                        </Katex>
+                    </li>
+                    <li>
+                        <Katex>
+                            {"\\dfrac{3}{7} + \\dfrac{5}{7} = \\dfrac{8}{7}"}
+                        </Katex>
+                    </li>
+                </ul>
+            </Exemples>
+        </SousPartie>
+        <SousPartie numero={2} titre="Fractions égales">
+            <Propriete>
+                Deux fractions sont égales si leur quotient sont égaux.
+            </Propriete>
+            <Exemples>
+                <ul>
+                    <li>
+                        <Katex>
+                            {"\\dfrac{3}{4} = \\dfrac{6}{8} ~~~~\\text{ car } 3 \\div 4 = \\boxed{0{,}75} ~~~~\\text{ et } 6 \\div 8 = \\boxed{0{,}75}"}
+                        </Katex>
+                    </li>
+                    <li>
+                        <Katex>
+                            {"\\dfrac{85}{99} = \\dfrac{850}{990} ~~~~\\text{ car } 85 \\div 99 = \\boxed{0{,}858585...} ~~~~\\text{ et } 850 \\div 990 = \\boxed{0{,}858585...}"}
+                        </Katex>
+                    </li>
+                </ul>
+            </Exemples>
+            <Propriete>
+                Si deux fractions sont égales, il existe un nombre avec lequel on peut multiplier 
+                le numérateur et le dénominateur de la première fraction pour obtenir la seconde.
+            </Propriete>
+            <Exemples>
+                <img    alt="Schéma expliquant les fractions égales"
+                        src="/tikz/6eme/ch05/fractions_egales.svg"
+                        style="height: 17ex" />
+            </Exemples>
+            <Definition>
+                Simplifier une fraction, c'est diviser le numérateur et le dénominateur par un même nombre.
+            </Definition>
+            <Exemples>
+                <ul>
+                    <li>
+                        <Katex>
+                            {"\\dfrac{10}{35} = \\dfrac{2 \\times \\cancel{5}}{7 \\times \\cancel{5}} = \\dfrac{2}{7}"}
+                        </Katex>
+                    </li>
+                </ul>
+            </Exemples>
+        </SousPartie>
     </Partie>
 </Chapitre>
 
