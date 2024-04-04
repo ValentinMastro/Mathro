55b522768efda275dc6635275bd94df5992f3718
Valentin Mastro <valentinmastro@hotmail.fr>
2024-04-04 15:08:14 +0200

    6eme ch18 diagrammes

--- www/src/routes/cours/6eme/chapitre18/+page.svelte
+++ www/src/routes/cours/6eme/chapitre18/+page.svelte
@@ -1,7 +1,52 @@
 <script lang="ts">
     import { Chapitre, Partie, SousPartie } from "$lib/chapitrage/all_chapitrages";
-    import { Definition, Exemples, Paragraphe } from "$lib/chapitrage/sections/all_sections";
+    import { Definition, Exemples, Paragraphe, Remarque } from "$lib/chapitrage/sections/all_sections";
     import { math, display } from 'mathlifier';
+    import DiagrammeCirculaire from "./DiagrammeCirculaire.svelte";
+
+    // Nombre d'habitant à Meaux par année
+    let donnees_1 = {
+        1982: 45005,
+        1990: 48305,
+        1999: 49421,
+        2007: 48653,
+        2013: 53766,
+        2019: 55750
+    }
+    // Prix moyen d'un kg de baguette de pain en France par année
+    // source : https://www.insee.fr/fr/statistiques/serie/000442423?idbank=000442423
+    let donnees_2 = {
+        "1992-01": 2.12,
+        "1997-01": 2.4,
+        "2002-01": 2.70,
+        "2007-01": 3.13,
+        "2012-01": 3.43,
+        "2017-01": 3.46,
+        "2022-01": 3.64,
+        "2023-11": 4.05
+    }
+    // Population de Meaux en 2019 par grandes tranches d'âge
+    let donnees_3 = {
+        "0-14": 12585,
+        "15-29": 11830,
+        "30-44": 11536,
+        "45-59": 9798,
+        "60-74": 6536,
+        "75 +": 3465,
+        "TOTAL": 55750
+    }
+    // Emploi selon la catégorie socioprofessionnelle en 2022
+    // source : https://www.insee.fr/fr/statistiques/2489546
+    let donnees_4 = {
+        "Agriculteurs": 1.6,
+        "Artisans": 6.8,
+        "Cadres": 21.7,
+        "Intermédiaires": 24.6,
+        "Employés": 26.0,
+        "Ouvriers": 18.9,
+        "?": 0.4,
+        "TOTAL": 100
+    }
 </script>
 
 <Chapitre numero={18} titre="Tableaux et diagrammes">
@@ -87,10 +132,128 @@
                     </tr>
                 </table>
 
-                <svg viewBox="0 0 500 200">
+                <svg viewBox="0 0 500 200" width="50em">
                     <line x1="50" y1="180" x2="450" y2="180" stroke="black" />
                     <line x1="50" y1="180" x2="50" y2="20" stroke="black" />
                 </svg>
+            </Exemples>
+        </SousPartie>
+        <SousPartie numero={2} titre="Diagramme en barres/en bâtons">
+            <Definition>
+                Un <i>diagramme en barres</i> permet de représenter des données par des barres de hauteurs proportionnelles.
+            </Definition>
+            <Remarque>
+                On l'utilise le plus souvent pour comparer des valeurs entre elles.
+            </Remarque>
+            <Exemples>
+                <ul class="liste_sans_marquage" style="padding: 0px; columns: 2;">
+                    <li>
+                        <table class="donnees">
+                            <caption>Nombre d'habitants à Meaux par année</caption>
+                            <tr>
+                                <th>Année</th>
+                                {#each Object.keys(donnees_1) as annee}
+                                    <th>{annee}</th>
+                                {/each}
+                            </tr>
+                            <tr>
+                                <th>Nombre d'habitants</th>
+                                {#each Object.values(donnees_1) as valeur}
+                                    <td>{valeur.toLocaleString("fr-FR")}</td>
+                                {/each}
+                            </tr>
+                        </table>
+                        <svg viewBox="0 0 1000 1000">
+                            <!-- Axes -->
+                            <line x1="100" y1="900" x2="100" y2="100" stroke="black" stroke-width="2" />
+                            <line x1="100" y1="900" x2="900" y2="900" stroke="black" stroke-width="2" />
+                            <!-- Graduations horizontales -->
+                            {#each Object.keys(donnees_1) as annees, i}
+                                <line x1={100 + 800*(i+0.5)/Object.keys(donnees_1).length} y1="900" x2={100 + 800*(i+0.5)/Object.keys(donnees_1).length} y2="910" stroke="black" stroke-width="2" />
+                                <text x={100 + 800*(i+0.5)/Object.keys(donnees_1).length} y="930" text-anchor="middle" font-size="22">{annees}</text>
+                            {/each}
+                            <!-- Graduations verticales -->
+                            {#each [0, 10000, 20000, 30000, 40000, 50000, 60000] as valeur, i}
+                                <line x1="90" y1={900 - 800*valeur/60000} x2="100" y2={900 - 800*valeur/60000} stroke="black" stroke-width="2" />
+                                <line x1="100" y1={900 - 800*valeur/60000} x2="900" y2={900 - 800*valeur/60000} stroke="black" stroke-width="1" stroke-dasharray="1,4" />
+                                <text x="80" y={900 - 800*valeur/60000 + 5} text-anchor="end" font-size="22">{valeur.toLocaleString("fr-FR")}</text>
+                            {/each}
+                            <!-- Barres -->
+                            {#each Object.values(donnees_1) as valeur, i}
+                                <rect x={100 + 800*(i+0.5)/Object.values(donnees_1).length - 20} y={900 - 800*valeur/60000} width="40" height={800*valeur/60000} fill="black" />
+                                <text x={100 + 800*(i+0.5)/Object.values(donnees_1).length} y={900 - 800*valeur/60000 - 10} text-anchor="middle" font-size="22">{valeur.toLocaleString("fr-FR")}</text>
+                            {/each}
+                            <!-- Légendes -->
+                            <text x="950" y="910" text-anchor="middle" font-size="22">Année</text>
+                            <text x="100" y="80" text-anchor="middle" font-size="22">Nombre d'habitants</text>
+                        </svg>
+                    </li>
+                    <li>
+                        <table class="donnees">
+                            <caption>Prix moyen d'un kg de baguette de pain en France</caption>
+                            <tr>
+                                <th>Année</th>
+                                {#each Object.keys(donnees_2) as annee}
+                                    <th>{annee}</th>
+                                {/each}
+                            </tr>
+                            <tr>
+                                <th style="width: fit-content">Prix</th>
+                                {#each Object.values(donnees_2) as valeur}
+                                    <td>{valeur.toLocaleString("fr-FR", {minimumFractionDigits: 2, maximumFractionDigits: 2})} €</td>
+                                {/each}
+                            </tr>
+                        </table>
+                        <svg viewBox="0 0 1000 1000">
+                            <!-- Axes -->
+                            <line x1="100" y1="900" x2="100" y2="100" stroke="black" stroke-width="2" />
+                            <line x1="100" y1="900" x2="900" y2="900" stroke="black" stroke-width="2" />
+                            <!-- Graduations horizontales -->
+                            {#each Object.keys(donnees_2) as annees, i}
+                                <line x1={100 + 800*(i+0.5)/Object.keys(donnees_2).length} y1="900" x2={100 + 800*(i+0.5)/Object.keys(donnees_2).length} y2="910" stroke="black" stroke-width="2" />
+                                <text x={100 + 800*(i+0.5)/Object.keys(donnees_2).length} y="930" text-anchor="middle" font-size="22">{annees}</text>
+                            {/each}
+                            <!-- Graduations verticales -->
+                            {#each [0, 1, 2, 3, 4, 5] as valeur, i}
+                                <line x1="90" y1={900 - 800*valeur/5} x2="100" y2={900 - 800*valeur/5} stroke="black" stroke-width="2" />
+                                <line x1="100" y1={900 - 800*valeur/5} x2="900" y2={900 - 800*valeur/5} stroke="black" stroke-width="1" stroke-dasharray="1,4" />
+                                <text x="80" y={900 - 800*valeur/5 + 5} text-anchor="end" font-size="22">{valeur.toLocaleString("fr-FR", {maximumFractionDigits: 2})} €</text>
+                            {/each}
+                            <!-- Barres -->
+                            {#each Object.values(donnees_2) as valeur, i}
+                                <rect x={100 + 800*(i+0.5)/Object.values(donnees_2).length - 20} y={900 - 800*valeur/5} width="40" height={800*valeur/5} fill="black" />
+                                <text x={100 + 800*(i+0.5)/Object.values(donnees_2).length} y={900 - 800*valeur/5 - 10} text-anchor="middle" font-size="22">{valeur.toLocaleString("fr-FR", {minimumFractionDigits: 2, maximumFractionDigits: 2})} €</text>
+                            {/each}
+                            <!-- Légendes -->
+                            <text x="950" y="910" text-anchor="middle" font-size="22">Année</text>
+                            <text x="100" y="80" text-anchor="middle" font-size="22">Prix (€)</text>
+                        </svg>
+                    </li>
+                </ul>
+            </Exemples>
+        </SousPartie>
+        <SousPartie numero={3} titre="Diagramme circulaire">
+            <Definition>
+                Un diagramme circulaire est un disque partagé en secteurs angulaires proportionnels aux valeurs qu'ils représentent.
+            </Definition>
+            <Remarque>
+                On l'utilise le plus souvent lorsque le total des valeurs est connu et pertinent.
+            </Remarque>
+            <Exemples>
+                <ul class="liste_sans_marquage" style="padding: 0px; columns: 2;">
+                    <li>
+                        <DiagrammeCirculaire donnees={donnees_3} width={1250}
+                        caption="Population de Meaux (77284) en 2019 par grandes tranches d'âge"
+                        etiquette_1="Tranche d'âge"
+                        etiquette_2="Nombre d'habitants" />
+                    </li>
+                    <li style="letter-spacing: -0.09em;">
+                        <DiagrammeCirculaire donnees={donnees_4} width={1250}
+                        caption="Emploi selon la catégorie socioprofessionnelle en 2022"
+                        etiquette_1="Catégorie"
+                        etiquette_2="Pourcentage" />
+                    </li>
+                </ul>
             </Exemples>
         </SousPartie>
     </Partie>
@@ -106,5 +269,20 @@
     table, tr, td {
         border: 1px solid black;
         padding: 5px;
+    }
+    .liste_sans_marquage {
+        list-style-type: none;
+    }
+    .donnees {
+        border-collapse: collapse;
+        border: 1px solid black;
+        width: 100%;
+        color: black;
+    }
+    .donnees th, .donnees td {
+        border: 1px solid black;
+        padding: 5px;
+        width: fit-content;
+        text-align: center;
     }
 </style>
