b33584067ed46f4576bd4b1e23b9e4a8aebb63fd
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-01 12:53:37 +0200

    Composant de cours

---
+++ www/src/lib/cahier/composants/de_cours/Propriete.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    interface Props {
+        lignes?: number,
+        children?: any,
+    }
+    let { lignes = 1, children }: Props = $props();
+    import Paragraphe from "$lib/cahier/composants/de_cours/Paragraphe.svelte";
+</script>
+
+<Paragraphe {lignes} nom_du_paragraphe="Propriété" couleur="rgb(225, 0, 0)">
+    {@render children()}
+</Paragraphe>
