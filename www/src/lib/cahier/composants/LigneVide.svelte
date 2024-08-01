8cdcd09ea1ad32161afca990ce68d63401fed4c4
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-01 15:54:33 +0200

    LigneVide peut s'étendre sur plusieurs lignes

--- www/src/lib/cahier/composants/LigneVide.svelte
+++ www/src/lib/cahier/composants/LigneVide.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+    interface Props {
+        lignes?: number,
+    }
+    let { lignes = 1 }: Props = $props();
     import { hauteur_ligne_cahier } from "$lib/cahier/store";
 </script>
 
 <svelte:options runes={true} />
 
-<div class="ligne-vide" style="height: {$hauteur_ligne_cahier}px; width: 100%;"></div>
+<div class="ligne-vide" style="height: {$hauteur_ligne_cahier * lignes}px; width: 100%;"></div>
