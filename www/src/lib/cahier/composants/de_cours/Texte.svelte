ea46b8caf82e5fdf1f26c9988ce9363a33b0639c
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-01 23:29:27 +0200

    Nouveau composant de cours : Texte.

---
+++ www/src/lib/cahier/composants/de_cours/Texte.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    interface Props {
+        addStyle?: string,
+        lignes?: number,
+        children?: any,
+    }
+    let { addStyle = "", lignes = 1, children }: Props = $props();
+    import { hauteur_ligne_cahier, taille_texte, separateur_texte } from "$lib/cahier/store";
+</script>
+
+<div class="texte" style="height: {$hauteur_ligne_cahier * lignes}px; line-height: {$separateur_texte}px; {addStyle}">
+    <span style="font-size: {$taille_texte}px;">{@render children()}</span>
+</div>
+
+<style>
+    .texte {
+        width: 100%;
+        position: relative;
+    }
+    span {
+        position: absolute;
+        bottom: 0;
+        margin-bottom: -0.7ex;
+    }
+</style>
