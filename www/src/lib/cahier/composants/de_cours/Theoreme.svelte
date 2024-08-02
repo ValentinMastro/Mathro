32882e4cf812c1a0c3e1492e31cd8764d206b09a
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-02 11:23:38 +0200

    Nouveau composant de cours : Théorème

---
+++ www/src/lib/cahier/composants/de_cours/Theoreme.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    interface Props {
+        nom_du_theoreme?: string,
+        lignes?: number,
+        children?: any,
+    }
+    let { nom_du_theoreme = "Théorème", lignes = 1, children }: Props = $props();
+    import Paragraphe from "$lib/cahier/composants/de_cours/Paragraphe.svelte";
+</script>
+
+<Paragraphe {lignes} nom_du_paragraphe={nom_du_theoreme} couleur="rgb(225, 0, 0)">
+    {@render children()}
+</Paragraphe>
