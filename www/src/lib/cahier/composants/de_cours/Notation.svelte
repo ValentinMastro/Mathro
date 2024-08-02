bbc90a7a8efd4dd712959ee16e643a9a8683893d
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-02 23:03:58 +0200

    Nouveau composant de cours : Notation

---
+++ www/src/lib/cahier/composants/de_cours/Notation.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    interface Props {
+        lignes?: number,
+        children?: any,
+    }
+    let { lignes = 1, children }: Props = $props();
+    import Paragraphe from "$lib/cahier/composants/de_cours/Paragraphe.svelte";
+</script>
+
+<Paragraphe {lignes} nom_du_paragraphe="Notation" couleur="black">
+    {@render children()}
+</Paragraphe>
