10447f0b85da7e39417a474798ee362a8c614ba6
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-03 22:16:31 +0200

    Factorisation et amélioration de la mise en page, avec l'utilisation de la marge pour les éléments intéractifs

--- www/src/lib/cahier/contenu/4eme/Page010.svelte
+++ www/src/lib/cahier/contenu/4eme/Page010.svelte
@@ -2,6 +2,7 @@
     import Chapitre from "$lib/cahier/composants/de_chapitrage/Chapitre.svelte";
     import Contenu from "$lib/cahier/composants/de_chapitrage/Contenu.svelte";
     import Partie from "$lib/cahier/composants/de_chapitrage/Partie.svelte";
+    import DansLaMarge from "$lib/cahier/composants/de_chapitrage/DansLaMarge.svelte";
 
     import Definition from "$lib/cahier/composants/de_cours/Definition.svelte";
     import Propriete from "$lib/cahier/composants/de_cours/Propriete.svelte";
@@ -9,21 +10,50 @@
     import Notation from "$lib/cahier/composants/de_cours/Notation.svelte";
     import Item from "$lib/cahier/composants/de_cours/Item.svelte";
     import Schema from "$lib/cahier/composants/de_cours/Schema.svelte";
-    import Ligne from "$lib/cahier/composants/de_cours/Ligne.svelte";
 
     import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
 
     import { math, display } from 'mathlifier';
+	import Ligne from "$lib/cahier/composants/de_cours/Ligne.svelte";
 
     function en_radians(deg: number): number {
         return deg * Math.PI / 180;
     }
 
     let facteur_1: number = $state(2);
-    let facteur_2: number = $state(1);
+    let facteur_2: number = $state(5);
 </script>
 
+{#snippet cercle(cx: number, cy: number, r: number, numerateur: number, denominateur: number, facteur: number)}
+    {#each Array(denominateur*facteur) as _, i}
+        <path 
+            d="M {cx} {cy} L {cx+r} {cy} A {r} {r} 0 0 0 {cx + r * Math.cos(en_radians((360/denominateur)/facteur))} {cy - r * Math.sin(en_radians((360/denominateur)/facteur))} Z"
+            transform-origin="{cx} {cy}"
+            transform={`rotate(-${i * (360/denominateur)/facteur})`}
+            stroke="black"
+            stroke-width={3}
+            fill={i + 1 <= numerateur*facteur ? 'green' : 'red'}
+        />
+        <text 
+            x={cx + (r+50+(i%2)*70) * Math.cos(en_radians((i+0.5)*(360/denominateur)/facteur))}
+            y={cy - (r+50+(i%2)*70) * Math.sin(en_radians((i+0.5)*(360/denominateur)/facteur))}
+            font-size={i % 2 == 1 ? 70 : 40} 
+            fill="{i + 1 <= numerateur*facteur ? 'green' : 'red'}"
+            text-anchor="middle" 
+            dominant-baseline="middle"
+        >
+            {i + 1}
+        </text>
+    {/each}
+{/snippet}
+
 <Chapitre titre="Fractions et proportionnalité" />
+
+<DansLaMarge apres_un_titre>
+    <LigneVide lignes={23} />
+    <input type="range" min="1" max="10" bind:value={facteur_1} style="width: 80%" />
+    <input type="range" min="1" max="5" bind:value={facteur_2} style="width: 80%" />
+</DansLaMarge>
 
 <Contenu apres_un_titre>
     <Partie numero={1} titre="Calculs fractionnaires" />
@@ -55,67 +85,17 @@
 
     <LigneVide lignes={0.6} />
 
-    <Exemples lignes={1}>
-        {@html  display("\\dfrac{3}{4} = \\dfrac{2 \\times 3}{2 \\times 4} = \\dfrac{6}{8} \\hspace{2cm} \\dfrac{35}{75} = \\dfrac{35 \\div 5}{75 \\div 5} = \\dfrac{7}{15}")}
-    </Exemples>
+    <Exemples />
 
-    <LigneVide />
-
-    <Schema lignes={10} aspectRatioSVG={1.25}>
+    <Schema lignes={8} aspectRatioSVG={2.4}>
         {#snippet svg()}
-            {#each Array(4*facteur_1) as _, i}
-                <path 
-                    d="M 300 300 L {300+250} 300 A 250 250 0 0 0 {300 + 250 * Math.cos(en_radians(90/facteur_1))} {300 - 250 * Math.sin(en_radians(90/facteur_1))} Z"
-                    transform-origin="300 300"
-                    transform={`rotate(-${i * 90/facteur_1})`}
-                    stroke="black"
-                    stroke-width={3}
-                    fill={i + 1 <= 3*facteur_1 ? 'green' : 'red'}
-                />
-                <text 
-                    x={300 + 280 * Math.cos(en_radians((i+0.5)*90/facteur_1))}
-                    y={300 - 280 * Math.sin(en_radians((i+0.5)*90/facteur_1))}
-                    font-size={i % 2 == 1 ? 50 : 35} 
-                    fill="{i + 1 <= 3*facteur_1 ? 'green' : 'red'}"
-                    text-anchor="middle" 
-                    dominant-baseline="middle"
-                >
-                    {i + 1}
-                </text>
-            {/each}
-            {#each Array(15*facteur_2) as _, i}
-                <path 
-                    d="M 900 650 L {900+250} 650 A 250 250 0 0 0 {900 + 250 * Math.cos(en_radians(24/facteur_2))} {650 - 250 * Math.sin(en_radians(24/facteur_2))} Z"
-                    transform-origin="900 650"
-                    transform={`rotate(-${i * 24/facteur_2})`}
-                    stroke="black"
-                    stroke-width={3}
-                    fill={i + 1 <= 7*facteur_2 ? 'green' : 'red'}
-                /> 
-                <text 
-                    x={900 + (i % 2 == 1 ? 320 : 280) * Math.cos(en_radians((i+0.5)*24/facteur_2))}
-                    y={650 - (i % 2 == 1 ? 320 : 280) * Math.sin(en_radians((i+0.5)*24/facteur_2))}
-                    font-size={i % 2 == 1 ? 50 : 35} 
-                    fill="{i + 1 <= 7*facteur_2 ? 'green' : 'red'}"
-                    text-anchor="middle" 
-                    dominant-baseline="middle"
-                >
-                    {i + 1}
-                </text>
-            {/each}
-        {/snippet}
-        {#snippet html()}
-            <input type="range" min="1" max="10" bind:value={facteur_1} style="width: 100%;"/>
-            <input type="range" min="1" max="5" bind:value={facteur_2} style="width: 100%;"/>
-
-            <LigneVide />
-            <Item>
-                {@html math(`\\dfrac{3}{4} = \\dfrac{${facteur_1} \\times 3}{${facteur_1} \\times 4} = \\dfrac{${facteur_1 * 3}}{${facteur_1 * 4}}`)}
-            </Item>
-            <LigneVide />
-            <Item>
-                {@html math(`\\dfrac{7}{15} = \\dfrac{7 \\times ${facteur_2}}{15 \\times ${facteur_2}} = \\dfrac{${facteur_2 * 7}}{${facteur_2 * 15}}`)}
-            </Item>
+            {@render cercle(500, 500, 300, 3, 4, facteur_1)}
+            {@render cercle(1750, 500, 300, 7, 15, facteur_2)}
         {/snippet}
     </Schema>
+
+    <LigneVide />
+    <Ligne>
+        {@html math(`\\hspace{1cm} \\dfrac{3}{4} = \\dfrac{${facteur_1} \\times 3}{${facteur_1} \\times 4} = \\dfrac{${facteur_1 * 3}}{${facteur_1 * 4}} \\hspace{3cm} \\dfrac{7}{15} = \\dfrac{7 \\times ${facteur_2}}{15 \\times ${facteur_2}} = \\dfrac{${facteur_2 * 7}}{${facteur_2 * 15}}`)}
+    </Ligne>
 </Contenu>
