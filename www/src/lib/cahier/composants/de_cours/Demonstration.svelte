694abab7a213d3c5b694d0007ceb98ac870d48fa
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-04 13:56:02 +0200

    Composant de cours : Démonstration

---
+++ www/src/lib/cahier/composants/de_cours/Demonstration.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    interface Props {
+        lignes?: number,
+        children?: any,
+    }
+    let { lignes = 1, children }: Props = $props();
+    import Paragraphe from "$lib/cahier/composants/de_cours/Paragraphe.svelte";
+</script>
+
+<Paragraphe {lignes} nom_du_paragraphe="Démonstration" couleur="rgb(0, 0, 138)">
+    {@render children()}
+</Paragraphe>
