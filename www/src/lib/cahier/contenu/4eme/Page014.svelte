81cb8cef444aaaf2d54626ee2dca3ec0ea7b0465
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-04 16:40:18 +0200

    page14 4eme

---
+++ www/src/lib/cahier/contenu/4eme/Page014.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import Contenu from "$lib/cahier/composants/de_chapitrage/Contenu.svelte";
+    import Partie from "$lib/cahier/composants/de_chapitrage/Partie.svelte";
+
+    import Definition from "$lib/cahier/composants/de_cours/Definition.svelte";
+    import Exemples from "$lib/cahier/composants/de_cours/Exemples.svelte";
+    import Item from "$lib/cahier/composants/de_cours/Item.svelte";
+
+    import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+
+    import { math } from "mathlifier";
+</script>
+
+<Contenu>
+    <Partie numero={3} titre={"Proportionnalité"} />
+        <LigneVide />
+        <Definition lignes={2}>
+            Une grandeur est une caractéristique quantifiable, associée à une unité.
+        </Definition>
+        <Exemples lignes={2}>
+            Distance (m), Prix (€), Durée (s), Température (°C), Masse (kg), <br>
+            Énergie (J), Puissance (W), Tension électrique (V), etc.
+        </Exemples>
+        <LigneVide />
+        <Definition lignes={3}>
+            Deux grandeurs sont proportionnelles s'il existe un coefficient de proportionnalité,
+            c'est-à-dire un nombre qui, multiplié à l'une des grandeurs, donne l'autre.
+        </Definition>
+        <Exemples lignes={6}>
+            <Item lignes={3}>
+                Le côté d'un carré et son périmètre sont proportionnels. <br>
+                En effet, {@html math("\\text{P}_{\\text{carré}} = 4 \\times c")} où {@html math("c")} est la longueur du côté.<br>
+                4 est donc le coefficient de proportionnalité.
+            </Item>
+            <Item lignes={3}>
+                L'âge et la taille d'une personne ne sont pas proportionnels.<br>
+                En effet, si une personne a 10 ans et qu'elle mesure 1,20 m, cela ne signifie 
+                pas qu'à 20 ans, elle mesurera 2,40 m.
+            </Item>
+        </Exemples>
+</Contenu>
