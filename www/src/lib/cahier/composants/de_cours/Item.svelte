d14ebe67d04fc7ddde76b78d7333fca519b66498
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-09 11:41:01 +0200

    On peut maintenant changer la couleur de la bullet de Item

--- www/src/lib/cahier/composants/de_cours/Item.svelte
+++ www/src/lib/cahier/composants/de_cours/Item.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
     interface Props {
         bullet?: string,
+        bullet_color?: string,
         lignes?: number,
         children?: any,
     }
-    let { bullet = "&bullet;", lignes = 1, children }: Props = $props();
+    let { bullet = "&bullet;", bullet_color = undefined, lignes = 1, children }: Props = $props();
     import { hauteur_ligne_cahier, taille_texte } from "$lib/cahier/store";
 </script>
 
 <div class="item" style="height: {$hauteur_ligne_cahier * lignes}px;">
-    <span style="font-size: {$taille_texte}px;">{@html bullet}</span> 
+    {#if bullet_color}
+        <span style="font-size: {$taille_texte}px; color: {bullet_color}">{@html bullet}</span>
+    {:else}
+        <span style="font-size: {$taille_texte}px;">{@html bullet}</span> 
+    {/if}
     <span style="font-size: {$taille_texte}px;">{@render children()}</span>
 </div>
 
