af70c840c2749e303d7e022ae2aa5b266fee8698
Valentin Mastro <valentinmastro@hotmail.fr>
2024-08-10 23:54:50 +0200

    SousSousPartie

--- www/src/lib/cahier/composants/de_chapitrage/*.ts
+++ www/src/lib/cahier/composants/de_chapitrage/*.ts
@@ -1,6 +1,7 @@
 export { default as Chapitre } from "./Chapitre.svelte";
 export { default as Partie } from "./Partie.svelte";
 export { default as SousPartie } from "./SousPartie.svelte";
+export { default as SousSousPartie } from "./SousSousPartie.svelte";
 
 export { default as Contenu } from "./Contenu.svelte";
 export { default as DansLaMarge } from "./DansLaMarge.svelte";
---
+++ www/src/lib/cahier/composants/de_chapitrage/SousSousPartie.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    interface Props {
+        numero: number,
+        titre: string,
+    }
+
+    let { numero, titre }: Props = $props();
+
+    import { hauteur_ligne_cahier, taille_titre_de_sections } from "$lib/cahier/store";
+
+    function enChiffresRomains(n: number) {
+        switch (n) {
+            case 1: return "i";
+            case 2: return "ii";
+            case 3: return "iii";
+            case 4: return "iv";
+            case 5: return "v";
+            case 6: return "vi";
+            case 7: return "vii";
+            case 8: return "viii";
+            case 9: return "ix";
+            case 10: return "x";
+            default: return n.toString();
+        }
+    }
+</script>
+
+<svelte:options runes={true} />
+
+<div class="partie" style="height: {$hauteur_ligne_cahier}px;">
+    <span style="font-size: {$taille_titre_de_sections*0.85}px;">
+        {enChiffresRomains(numero)}) {titre}
+    </span>
+</div>
+
+
+<style>
+    .partie {
+        width: 100%;
+        color: rgb(0, 0, 138);
+        position: relative;
+    }
+    span {
+        position: absolute;
+        bottom: 0;
+        margin-bottom: -0.25em;
+    }
+</style>
