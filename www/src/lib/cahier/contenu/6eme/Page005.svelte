2f40a23ff0228d46af44e3f69de0b6f455be4972
ValentinMastro <47474745+ValentinMastro@users.noreply.github.com>
2024-09-03 14:33:38 +0000

    début page 005 6ème

---
+++ www/src/lib/cahier/contenu/6eme/Page005.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Contenu, Partie } from '$lib/cahier/composants/de_chapitrage/*';
+    import { Propriete, Item, Remarque, Exemple, Exemples } from '$lib/cahier/composants/de_cours/*';
+</script>
+
+<Contenu>
+    <Partie numero={3} titre="Repérage" />
+        
+</Contenu>
