a32f14bda31066a36f638992d48ac9573d8d25c1
ValentinMastro <47474745+ValentinMastro@users.noreply.github.com>
2024-09-05 08:07:35 +0000

    pages vides 5èmes

---
+++ www/src/lib/cahier/contenu/5eme/Page001.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import PageDeGarde from "$lib/cahier/composants/PageDeGarde.svelte";
+</script>
+
+<PageDeGarde />
---
+++ www/src/lib/cahier/contenu/5eme/Page002.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Sommaire from "$lib/cahier/composants/Sommaire.svelte"
+</script>
+
+<Sommaire />
