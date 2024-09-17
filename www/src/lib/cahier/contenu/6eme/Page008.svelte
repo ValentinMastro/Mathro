2c95427678f6f28f83e6b2a8c51a54b18b100cc5
Valentin Mastro <valentinmastro@hotmail.fr>
2024-09-17 11:43:50 +0200

    page8 6eme

--- www/src/lib/cahier/contenu/6eme/Page008.svelte
+++ www/src/lib/cahier/contenu/6eme/Page008.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
-    import { Contenu, Partie } from "$lib/cahier/composants/de_chapitrage/*";
+    import { Contenu, Partie, DansLaMarge } from "$lib/cahier/composants/de_chapitrage/*";
     import { Definition, Notation, Exemples, Schema, Item, Exemple } from "$lib/cahier/composants/de_cours/*";
     import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
 	import { math } from "mathlifier";
+
+    let t = $state(0);
+    let point: {x: number, y: number} = $derived({
+        x: 200 + (3600-200)*t,
+        y: 200 + (800-200)*t
+    });
 </script>
+
+<DansLaMarge>
+    <LigneVide lignes={20} />
+    <input type="range" min="0" max="1" step="0.01" bind:value={t} style="width: 80%;" />
+</DansLaMarge>
 
 <Contenu>
     <Partie numero={2} titre="Alignement et appartenance" />
         <LigneVide />
+        <Definition lignes={2}>
+            Trois points sont alignés s'il est possible de les placer sur la même droite.
+        </Definition>
         <Notation lignes={2}>
             Si le point M est sur la droite (AB), on note {@html math("\\text{M} \\in (\\text{AB})")}.<br>
             Si le point A n'est pas sur le segment [BC], on note {@html math("\\text{A} \\notin [\\text{BC}]")}.
@@ -19,7 +33,24 @@
                 <line x1="300" x2="300" y1="170" y2="270" stroke="black" stroke-width="10"></line>
                 <line x1="2500" x2="2500" y1="570" y2="670" stroke="black" stroke-width="10"></line>
                 <!-- line qui passe par 2500 600 -->
-                <line x1="2500" x2="3000" y1="600" y2="600" stroke="black" stroke-width="10"></line>
+                <line x1="2500" x2="3200" y1="600" y2="300" stroke="black" stroke-width="10"></line>
+                <line x1="3200" x2="3200" y1="250" y2="350" stroke="black" stroke-width="10"></line>
+                <!-- Nom des points -->
+                <text x={200} y={400} font-size="150" fill="black">A</text>
+                <text x={2500} y={780} font-size="150" fill="black">B</text>
+                <text x={3200} y={200} font-size="150" fill="black">C</text>
+            {/snippet}
+        </Schema>
+        <LigneVide lignes={4} />
+        <Schema lignes={5} aspectRatioSVG={4}>
+            {#snippet svg()}
+                <line x1="200" y1="200" x2="3600" y2="800" stroke="black" stroke-width="10"></line>
+                <line x1="1000" x2="1000" y1="300" y2="400" stroke="black" stroke-width="10"></line>
+                <line x1="2500" x2="2500" y1="570" y2="670" stroke="black" stroke-width="10"></line>
+                <text x="1000" y="570" font-size="150" text-anchor="middle" fill="black">M</text>
+                <text x="2500" y="800" font-size="150" text-anchor="middle" fill="black">P</text>
+                <circle cx={point.x} cy={point.y} r="35" fill="red"></circle>
+                <text x={point.x} y={point.y + 100} font-size="150" text-anchor="middle" fill="black">N</text>
             {/snippet}
         </Schema>
 </Contenu>
