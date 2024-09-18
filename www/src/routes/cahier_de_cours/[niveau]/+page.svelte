aa2bc2f5119873423965fd1e6d463c2340df0143
Valentin Mastro <valentinmastro@hotmail.fr>
2024-09-18 20:59:40 +0200

    2 lignes de test enlevées

--- www/src/routes/cahier_de_cours/[niveau]/+page.svelte
+++ www/src/routes/cahier_de_cours/[niveau]/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-    import { page } from "$app/stores";
     import { onMount } from "svelte";
-    const url_numero_de_la_page = $page.url.searchParams.get("page");
 
     import type { PageData } from "./$types";
     export let data: PageData;
