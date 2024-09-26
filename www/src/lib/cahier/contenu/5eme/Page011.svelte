7418728f55f9f994e2f6e0df563d7fb10a472087
Valentin Mastro <valentinmastro@hotmail.fr>
2024-09-26 08:53:49 +0200

    pages 11, 12, 13 5ème

---
+++ www/src/lib/cahier/contenu/5eme/Page011.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { Chapitre, Contenu, DansLaMarge, Partie } from "$lib/cahier/composants/de_chapitrage/*";
+    import { TheoremeDefinition, Exemples, Item } from "$lib/cahier/composants/de_cours/*";
+	import { Nombre } from "$lib/cahier/composants/de_marge/*";
+    import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+	import { math } from "mathlifier";
+
+    let dividende = $state(1997);
+    let diviseur = $state(27);
+    let quotient = $derived(Math.floor(dividende / diviseur));
+    let reste = $derived(dividende % diviseur);
+</script>
+
+<Chapitre titre="Nombres premiers" />
+
+<DansLaMarge apres_un_titre>
+    <LigneVide lignes={10} />
+    <Nombre bind:valeur={dividende} label="dividende" min={1000} max={9999} />
+    <Nombre bind:valeur={diviseur} label="diviseur" min={10} max={99} />
+</DansLaMarge>
+
+<Contenu apres_un_titre>
+    <Partie numero={1} titre="Division euclidienne" />
+        <LigneVide />
+        <TheoremeDefinition lignes={5}>
+            À deux nombres entiers, le <i>dividende</i> et le <i>diviseur</i>, on associe deux autres nombres entiers, 
+            le <i>quotient</i> et le <i>reste</i>, <u>de manière unique</u>, tels que :
+                <Item>{@html math("\\text{dividende} = \\text{diviseur} \\times \\text{quotient} + \\text{reste}")}</Item>
+                <Item>{@html math("0 \\leq \\text{reste} < \\text{diviseur}")}</Item>
+        </TheoremeDefinition>
+        <Exemples lignes={5}>
+            <Item>{@html math(`${dividende} = ${diviseur} \\times ${quotient} + ${reste}`)}</Item>
+            <Item>le dividende est {dividende}</Item>
+            <Item>le diviseur est {diviseur}</Item>
+            <Item>le quotient est {quotient}</Item>
+            <Item>le reste est {reste}</Item>
+        </Exemples>
+</Contenu>
---
+++ www/src/lib/cahier/contenu/5eme/Page012.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { Partie, Contenu } from "$lib/cahier/composants/de_chapitrage/*";
+    import { Definition, Exemples, Notation, Paragraphe, Item, Tableau, LigneTableau, Methode } from "$lib/cahier/composants/de_cours/*";
+    import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+</script>
+
+<Contenu>
+    <Partie numero={2} titre="Divisibilités" />
+        <LigneVide />
+        <Definition lignes={2}>
+            "Être divisible par" signifie "être dans la table de multiplication de".
+        </Definition>
+        <Exemples lignes={2}>
+            <Item>42 est divisible par <span class="rouge">6</span> car 42 = <span class="rouge">6</span> × 7</Item>
+            <Item>35 est divisible par <span class="rouge">5</span> car 35 = <span class="rouge">5</span> × 7</Item>
+        </Exemples>
+        <Notation lignes={5}>
+            On peut dire que : 
+                <Item>42 est divisible par 6</Item>
+                <Item>42 est un multiple de 6</Item>
+                <Item>6 divise 42</Item>
+                <Item>6 est un diviseur de 42</Item>
+        </Notation>
+        <LigneVide />
+        <Methode lignes={4}>
+            Pour savoir si un nombre est divisible par un autre, on peut :
+                <Item lignes={2}>calculer le reste de la division euclidienne, et vérifier qu'il est égal à 0</Item>
+                <Item>utiliser les critères de divisibilité</Item>
+        </Methode>
+</Contenu>
