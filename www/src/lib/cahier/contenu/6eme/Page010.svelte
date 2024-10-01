db0f99881d6dd8f6d4e9e2b9608152c05f35470c
Valentin Mastro <valentinmastro@hotmail.fr>
2024-10-01 06:45:11 +0200

    page 10 non finie

--- www/src/lib/cahier/contenu/6eme/Page010.svelte
+++ www/src/lib/cahier/contenu/6eme/Page010.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Contenu, Partie } from "$lib/cahier/composants/de_chapitrage/*";
-	import { Propriete } from "$lib/cahier/composants/de_cours/*";
+	import { Exemples, Propriete } from "$lib/cahier/composants/de_cours/*";
     import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
 </script>
 
@@ -11,4 +11,8 @@
             Si on multiplie le numérateur et le dénominateur <u>par le même nombre</u>,
             alors on obtient une deuxième fraction égale à la première.
         </Propriete>
+        <LigneVide />
+        <Exemples>
+            
+        </Exemples>
 </Contenu>
