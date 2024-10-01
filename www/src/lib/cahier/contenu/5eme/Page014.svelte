46642eb0333eff44309952662cd36af0747429ad
Valentin Mastro <valentinmastro@hotmail.fr>
2024-10-01 16:39:50 +0200

    pages 14 et 15 5ème

---
+++ www/src/lib/cahier/contenu/5eme/Page014.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Contenu, Partie } from "$lib/cahier/composants/de_chapitrage/*";
+	import { Definition, Item, Paragraphe, Theoreme, Exemples } from "$lib/cahier/composants/de_cours/*";
+	import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+    import { math } from "mathlifier";
+</script>
+
+<Contenu>
+    <Partie numero={3} titre="Les nombres premiers" />
+        <LigneVide />
+        <Definition lignes={2}>
+            Un nombre premier est un nombre qui a exactement deux diviseurs : 1 et lui-même.
+        </Definition>
+        <Paragraphe nom_du_paragraphe="Convention">
+            1 n'est pas un nombre premier.
+        </Paragraphe>
+        <LigneVide />
+        <Paragraphe nom_du_paragraphe="Liste de nombres premiers à connaître">
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37
+        </Paragraphe>
+        <LigneVide />
+        <Theoreme nom_du_theoreme="Théorème fondamental de l'arithmétique" lignes={2}>
+            Tout entier supérieur ou égal à 2 peut s'écrire comme un <i>unique</i> produit croissant 
+            de nombres premiers.    
+        </Theoreme>
+        <Exemples lignes={4}>
+            <Item>
+                {@html math("12 = 2 \\times 2 \\times 3")}
+            </Item>
+            <Item>
+                {@html math("540 = 2 \\times 2 \\times 3 \\times 3 \\times 3 \\times 5")}
+            </Item>
+            <Item>
+                {@html math("1001 = 7 \\times 11 \\times 13")}
+            </Item>
+            <Item>
+                {@html math("2310 = 2 \\times 3 \\times 5 \\times 7 \\times 11")}
+            </Item>
+        </Exemples>
+</Contenu>
---
+++ www/src/lib/cahier/contenu/5eme/Page015.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Contenu, Partie } from "$lib/cahier/composants/de_chapitrage/*";
+	import { Exemples, Methode, Item } from "$lib/cahier/composants/de_cours/*";
+    import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+	import { math } from "mathlifier";
+</script>
+
+<Contenu>
+    <Partie numero={4} titre="Application du théorème : simplifier des fractions" />
+        <LigneVide />
+        <Methode lignes={3}>
+            Pour simplifier une fraction, on peut écrire la décomposition 
+            en produit de facteurs premiers du numérateur et du dénominateur,
+            puis simplifier en divisant par les facteurs communs.
+        </Methode>
+        <LigneVide />
+        <Exemples lignes={9}>
+            <Item lignes={4}>
+                Simplifier la fraction {@html math("\\dfrac{12}{18}")}.<br>
+                {@html math("12 = 2 \\times 2 \\times 3")} et {@html math("18 = 2 \\times 3 \\times 3")}.<br>
+                On a donc {@html math("\\frac{12}{18} = \\frac{2 \\times 2 \\times 3}{2 \\times 3 \\times 3}")}.<br>
+                En simplifiant par les facteurs communs, on obtient {@html math("\\frac{12}{18} = \\frac{2}{3}")}.
+            </Item>
+            <LigneVide />
+            <Item lignes={4}>
+                Simplifier la fraction {@html math("\\dfrac{540}{720}")}.<br>
+                {@html math("540 = 2 \\times 2 \\times 3 \\times 3 \\times 3 \\times 5")} et {@html math("720 = 2 \\times 2 \\times 2 \\times 3 \\times 3 \\times 5")}.<br>
+                On a donc {@html math("\\frac{540}{720} = \\frac{2 \\times 2 \\times 3 \\times 3 \\times 3 \\times 5}{2 \\times 2 \\times 2 \\times 3 \\times 3 \\times 5}")}.<br>
+                En simplifiant par les facteurs communs, on obtient {@html math("\\frac{540}{720} = \\frac{3}{4}")}.
+            </Item>
+        </Exemples>
+</Contenu>
