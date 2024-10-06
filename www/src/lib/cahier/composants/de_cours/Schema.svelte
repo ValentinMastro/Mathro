5a55f9a95ea77fbe0b53ff247291c64abc1aa0fa
Valentin Mastro <valentinmastro@hotmail.fr>
2024-10-06 22:18:51 +0200

    Factorisation du composant Schema

--- www/src/lib/cahier/composants/de_cours/Schema.svelte
+++ www/src/lib/cahier/composants/de_cours/Schema.svelte
@@ -4,32 +4,35 @@
         aspectRatioSVG?: number,
         svg?: any,
         html?: any,
+        position_html?: 'gauche' | 'droite',
     }
-    let { lignes = 1, aspectRatioSVG = 1/1, svg, html }: Props = $props();
+    let { lignes = 1, aspectRatioSVG = 1/1, svg, html, position_html = 'droite' }: Props = $props();
     import { hauteur_ligne_cahier } from "$lib/cahier/store";
 </script>
 
-<div 
-    class="schema" 
-    style="height: {$hauteur_ligne_cahier * lignes}px;"
->
+{#snippet afficher_svg()}
     {#if svg}
-        <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            viewBox="0 0 {1000*aspectRatioSVG} 1000"
-            height={$hauteur_ligne_cahier * lignes}
-            width={$hauteur_ligne_cahier * lignes * aspectRatioSVG}
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {1000*aspectRatioSVG} 1000">
             {@render svg()}
         </svg>
     {/if}
+{/snippet}
+    
+{#snippet afficher_html()}
     {#if html}
-        <div 
-            class="contenu_html"
-            style="height: {$hauteur_ligne_cahier * lignes}px; width: calc(100% - {$hauteur_ligne_cahier * lignes * aspectRatioSVG}px);"
-        >
+        <div id="html">
             {@render html()}
         </div>
+    {/if}
+{/snippet}
+
+<div class="schema" style="--hauteur: {$hauteur_ligne_cahier}px; --lignes: {lignes}; --aspectRatioSVG: {aspectRatioSVG}">
+    {#if position_html === 'gauche'}
+        {@render afficher_html()}
+        {@render afficher_svg()}
+    {:else}
+        {@render afficher_svg()}
+        {@render afficher_html()}
     {/if}
 </div>
 
@@ -37,6 +40,18 @@
     .schema {
         display: flex;
         align-items: center;
-        width: 100%;
+        width: calc(100% * 80/79);
+        height: calc(var(--hauteur) * var(--lignes));
+
+        --largeur-svg: calc(var(--hauteur) * var(--lignes) * var(--aspectRatioSVG));
+        --largeur-html: calc(100% - var(--largeur-svg));
+    }
+    svg {
+        height: 100%;
+        width: var(--largeur-svg);
+    }
+    #html {
+        height: 100%;
+        width: var(--largeur-html);
     }
 </style>
