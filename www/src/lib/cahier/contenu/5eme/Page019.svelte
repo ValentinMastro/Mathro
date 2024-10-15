f958842ad865f054c5ff5c70501af20089742d60
Valentin Mastro <valentinmastro@hotmail.fr>
2024-10-15 22:34:16 +0200

    début page 19 5ème

--- www/src/lib/cahier/contenu/5eme/Page019.svelte
+++ www/src/lib/cahier/contenu/5eme/Page019.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
     import { Partie, Contenu } from "$lib/cahier/composants/de_chapitrage/*";
-	import { Definition } from "$lib/cahier/composants/de_cours/*";
-
+	import { Definition, Item, Notation, Remarque } from "$lib/cahier/composants/de_cours/*";
+	import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+	import { math } from "mathlifier";
 </script>
 
 <Contenu>
     <Partie numero={2} titre="Repérage sur une droite graduée" />
-        <Definition>
-            
+        <LigneVide />
+        <Definition lignes={5}>
+            Un repère orthogonal est constitué : 
+            <Item>
+                d'un point : <i>l'origine</i>
+            </Item>
+            <Item>
+                d'un axe horizontal : <i>l'axe des abscisses</i>
+            </Item>
+            <Item>
+                d'un axe vertical : <i>l'axe des ordonnées</i>
+            </Item>
+            Les deux axes sont perpendiculaires et se coupent en l'origine.
         </Definition>
+        <LigneVide />
+        <Definition lignes={2}>
+            À chaque point du repère, on associe deux nombres : son <i>abscisse</i> et son <i>ordonnée</i>.
+        </Definition>
+        <Notation>
+            Si le point A a pour abscisse {@html math('2')} et pour ordonnée {@html math('-3')}, on note {@html math('\\text{A}(2;-3)')}.
+        </Notation>
 </Contenu>
