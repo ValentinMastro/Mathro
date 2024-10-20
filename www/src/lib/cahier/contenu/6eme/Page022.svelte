6dc72c8c51e508d80b747c4c5ce6a2b2b8e90e90
Valentin Mastro <valentinmastro@hotmail.fr>
2024-10-20 11:56:04 +0200

    début ch6 6ème

---
+++ www/src/lib/cahier/contenu/6eme/Page022.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Chapitre, Contenu, Partie } from "$lib/cahier/composants/de_chapitrage/*";
+</script>
+
+<Chapitre titre="Droites" />
+
+<Contenu apres_un_titre>
+    <Partie numero={1} titre="Relations entre deux droites" />
+</Contenu>
+
