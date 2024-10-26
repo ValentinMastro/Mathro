702c21a040b491466fbae9c72c02959869780440
Valentin Mastro <valentinmastro@hotmail.fr>
2024-10-26 14:46:49 +0200

    debut page 27 6eme

---
+++ www/src/lib/cahier/contenu/6eme/Page027.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Chapitre, Contenu } from "$lib/cahier/composants/de_chapitrage/*";
+
+
+</script>
+
+<Chapitre titre="Addition, Soustraction et Multiplication" />
+
+<Contenu apres_un_titre>
+    
+</Contenu>
