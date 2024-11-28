7e32973ab5bc2ef043bfa1634cc06d794dbf49ac
Valentin Mastro <valentinmastro@hotmail.fr>
2024-11-28 18:40:19 +0100

    cours du 28/11

---
+++ www/src/lib/cahier/contenu/6eme/Page030.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Chapitre, Contenu, Partie } from "$lib/cahier/composants/de_chapitrage/*";
+	import { Definition, Item } from "$lib/cahier/composants/de_cours/*";
+	import Paragraphe from "$lib/cahier/composants/de_cours/Paragraphe.svelte";
+	import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+
+
+</script>
+
+<Chapitre titre="Polygones" />
+
+<Contenu apres_un_titre>
+    <Partie numero={1} titre="Les différents types de polygones" />
+        <LigneVide />
+        <Definition>
+            Un polygone est une figure fermée qui possède plusieurs côtés.
+        </Definition>
+        <Paragraphe nom_du_paragraphe="Nomenclature" lignes={6}>
+            <Item>
+                3 côtés : triangle
+            </Item>
+            <Item>
+                4 côtés : quadrilatère
+            </Item>
+            <Item>
+                5 côtés : pentagone
+            </Item>
+            <Item>
+                6 côtés : hexagone
+            </Item>
+            <Item>
+                8 côtés : octogone
+            </Item>
+            <Item>
+                10 côtés : décagone
+            </Item>
+        </Paragraphe>
+</Contenu>
---
+++ www/src/lib/cahier/contenu/6eme/Page032.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { Contenu, Partie, SousPartie } from "$lib/cahier/composants/de_chapitrage/*";
+	import { Definition, Schema, Propriete } from "$lib/cahier/composants/de_cours/*";
+    import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+</script>
+
+<Contenu>
+    <Partie numero={3} titre="Polygones particuliers" />
+        <SousPartie numero={1} titre="Les polygones réguliers" />
+            <LigneVide />
+            <Definition lignes={2}>
+                Un polygone est régulier si tous ses côtés ont la même longueur et si tous ses angles ont la même mesure.
+            </Definition>
+
+            <LigneVide />
+        <SousPartie numero={2} titre="Les différents types de triangles" />
+            <LigneVide />
+            <Definition>
+                Un triangle rectangle possède un angle droit.
+            </Definition>
+            <Schema lignes={5}>
+                {#snippet svg()}
+                    <defs>
+                        <polygon id="triangle_rectangle" points="200,200 800,200 800,800" />
+                    </defs>
+                    <use href="#triangle_rectangle" stroke="black" fill="none" stroke-width="8" />
+                    <rect fill="black" x={800-50} y={200} width={50} height={50} />
+                {/snippet}
+            </Schema>
+            <LigneVide />
+            <Definition>
+                Un triangle isocèle possède deux côtés de même longueur.
+            </Definition>
+            <Schema lignes={5} aspectRatioSVG={1.1}>
+                {#snippet svg()}
+                    <defs>
+                        <polygon id="triangle_isocèle" points="200,200 1000,200 600,800" />
+                    </defs>
+                    <use href="#triangle_isocèle" stroke="black" fill="none" stroke-width="8" />
+                    <text x={200} y={200} dx={-50} font-size={70}>A</text>
+                    <text x={1000} y={200} dx={50} font-size={70}>B</text>
+                    <text x={600} y={800} dy={60} font-size={70}>C</text>
+                {/snippet}
+            </Schema>
+            <LigneVide />
+            <Propriete>
+                Un triangle équilatéral possède trois angles de même mesure : 60°.
+            </Propriete>
+            <Schema lignes={5}>
+                {#snippet svg()}
+                    <defs>
+                        <polygon id="triangle_équilatéral" points="200,800 800,800 {200+600*Math.cos(Math.PI/3)},{800-600*Math.sin(Math.PI/3)}" />
+                    </defs>
+                    <use href="#triangle_équilatéral" stroke="black" fill="none" stroke-width="8" />
+                {/snippet}
+            </Schema>
+            
+</Contenu>
