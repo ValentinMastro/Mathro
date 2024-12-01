45667ec0ee24e155e4a965369ff1df2ef9ec658e
Valentin Mastro <valentinmastro@hotmail.fr>
2024-12-01 18:55:28 +0100

    Composant de cours : Nomenclature

---
+++ www/src/lib/cahier/composants/de_cours/Nomenclature.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { Paragraphe, type ParagrapheProps } from "./*";
+    let { addStyle = "", lignes = 1, children }: ParagrapheProps = $props();
+</script>
+
+<Paragraphe {lignes} nom_du_paragraphe="Nomenclature" couleur="black" {addStyle}>
+    {@render children()}
+</Paragraphe>
