c634df6b7967ab6aa084d4dcdc0f5a81fb64c2a0
ValentinMastro <47474745+ValentinMastro@users.noreply.github.com>
2024-12-06 09:06:03 +0000

    6èmes : polygones triangles et quadrilatères

--- www/src/lib/cahier/contenu/6eme/Page033.svelte
+++ www/src/lib/cahier/contenu/6eme/Page033.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-    import { Contenu } from "$lib/cahier/composants/de_chapitrage/*";
+    import { Contenu, SousPartie } from "$lib/cahier/composants/de_chapitrage/*";
     import { Definition, Propriete, Schema } from "$lib/cahier/composants/de_cours/*";
+	import Item from "$lib/cahier/composants/de_cours/Item.svelte";
     import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
 </script>
 
@@ -14,6 +15,24 @@
                 <polygon id="triangle_équilatéral" points="200,800 800,800 {200+600*Math.cos(Math.PI/3)},{800-600*Math.sin(Math.PI/3)}" />
             </defs>
             <use href="#triangle_équilatéral" stroke="black" fill="none" stroke-width="8" />
+            <text x={(200+800)/2} y={(800+800)/2} fill='red' font-size='80'>|</text> 
+            <text x={(200+200+600*Math.cos(Math.PI/3))/2} y={(800+800-600*Math.sin(Math.PI/3))/2} fill='red' font-size='80'>|</text>
+            <text x={(800+200+600*Math.cos(Math.PI/3))/2} y={(800+800-600*Math.sin(Math.PI/3))/2} fill='red' font-size='80'>|</text>
         {/snippet}
     </Schema>
+
+    <SousPartie numero={3} titre='Les différents types de quadrilatères' />
+        <LigneVide />
+        <Definition lignes={4}>
+            <Item>
+                Un rectangle possède 4 angles droits.
+            </Item>
+            <Item>
+                Un losange possède 4 côtés de même longueur.
+            </Item>
+            <Item lignes={2}>
+                Un carré est à la fois un rectangle et un losange : 
+                il possède 4 angles droits et 4 côtés de même longueur.
+            </Item>
+        </Definition>
 </Contenu>
