20b9da9f4149608e246b90dd35e385db2315e278
Valentin Mastro <valentinmastro@hotmail.fr>
2025-02-10 10:40:55 +0100

    Grandeurs 5ème

---
+++ www/src/lib/cahier/contenu/5eme/Page030.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import { Contenu, Partie } from "$lib/cahier/composants/de_chapitrage/*";
+	import { Paragraphe } from "$lib/cahier/composants/de_cours/*";
+	import { math, alignStar } from "mathlifier";
+    import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+	import Item from "$lib/cahier/composants/de_cours/Item.svelte";
+</script>
+
+<Contenu>
+    <Partie numero={2} titre="Exemples d'exercices" />
+    <LigneVide />
+
+    <Paragraphe nom_du_paragraphe="Énoncé" lignes={6}>
+        Un gâteau est coupé en 4 morceaux.<br>
+        Albert a {@html math("\\frac{1}{2}")} du gâteau.<br>
+        Bernard a {@html math("\\frac{1}{8}")} du gâteau.<br>
+        Camille a {@html math("\\frac{3}{16}")} du gâteau.<br>
+        Que reste-t-il pour Danielle ?<br>
+        <LigneVide />
+    </Paragraphe>
+    <Paragraphe nom_du_paragraphe="Solution" lignes={6}>
+        {@html alignStar(`
+            &\\dfrac{1}{\\color{green}1}
+            - \\dfrac{1}{2}
+            - \\dfrac{1}{8}
+            - \\dfrac{3}{16}
+            \\\\
+            &= 
+            \\dfrac{1 \\times 16}{1 \\times 16}
+            - \\dfrac{1 \\times 8}{2 \\times 8}
+            - \\dfrac{1 \\times 2}{8 \\times 2}
+            - \\dfrac{3}{16}
+            \\\\
+            &=
+            \\dfrac{16}{16}
+            - \\dfrac{8}{16}
+            - \\dfrac{2}{16}
+            - \\dfrac{3}{16}
+            \\\\
+            &= 
+            \\dfrac{3}{16}
+        `)}
+    </Paragraphe>
+    <Paragraphe nom_du_paragraphe="Énoncé" lignes={5}>
+        Sur mon disque dur : 
+        <Item>
+            {@html math("\\frac{1}{3}")} de l'espace est utilisé pour les vidéos.
+        </Item>
+        <Item>
+            {@html math("\\frac{1}{6}")} de l'espace est utilisé pour les images.
+        </Item>
+        <Item>
+            {@html math("\\frac{1}{24}")} de l'espace est utilisé pour des logiciels.
+        </Item>
+        Quelle fraction de l'espace disque n'est pas utilisé ?
+    </Paragraphe>
+    <LigneVide />
+    <Paragraphe nom_du_paragraphe="Solution" lignes={6}>
+        {@html alignStar(`
+            & 1 - \\frac{1}{3} - \\frac{1}{6} - \\frac{1}{24} 
+            \\\\
+            &= 
+            \\frac{1 \\times 24}{1 \\times 24} -\\frac{1 \\times 8}{3 \\times 8} - \\frac{1 \\times 4}{6 \\times 4} - \\frac{1}{24}
+            \\\\
+            &=
+            \\frac{24}{24} - \\frac{8}{24} - \\frac{4}{24} - \\frac{1}{24}
+            \\\\
+            &= 
+            \\frac{11}{24}
+        `)}
+    </Paragraphe>
+</Contenu>
---
+++ www/src/lib/cahier/contenu/5eme/Page032.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Contenu } from "$lib/cahier/composants/de_chapitrage/*";
+    import { Exemples } from "$lib/cahier/composants/de_cours/*";
+	import Item from "$lib/cahier/composants/de_cours/Item.svelte";
+	import Texte from "$lib/cahier/composants/de_cours/Texte.svelte";
+	import { alignStar } from "mathlifier";
+
+
+</script>
+
+<Contenu>
+    <Exemples lignes={5}>
+        <Item>
+            Calculer le périmètre d'un cercle de rayon 5 cm.
+        </Item>
+        <Texte lignes={4}>
+            {@html alignStar(`
+                𝒫_{\\text{cercle}} &= 2 \\times \\pi \\times \\text{rayon} \\\\
+                &= 2 \\times \\pi \\times 5 cm \\\\
+                &= 10\\pi cm \\longleftarrow \\text{valeur exacte} \\\\
+                &\\approx 31{,}4 cm \\longleftarrow \\text{valeur approchée} 
+            `)}
+        </Texte>
+    </Exemples>
+</Contenu>
