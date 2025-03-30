4572e287261899d4c4af2fb61c3aea12b924aeb9
Valentin Mastro <valentinmastro@hotmail.fr>
2025-03-30 01:46:00 +0100

    TheoremeDefinition -> bonne couleur + utilisation du prop children

--- www/src/lib/cahier/composants/de_cours/TheoremeDefinition.svelte
+++ www/src/lib/cahier/composants/de_cours/TheoremeDefinition.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <Paragraphe 
-        {lignes} 
-        nom_du_paragraphe="Théorème-Définition" 
-        couleur="rgb(225, 0, 0)" 
-        addStyle={addStyle + "background: linear-gradient(90deg, rgba(225,0,0,1) 50%, rgba(0,138,0,1) 50%); background-clip: text; color: transparent;"}>
-    {@render children()}
-</Paragraphe>
+    {lignes} 
+    nom_du_paragraphe="Théorème-Définition" 
+    couleur="rouge" 
+    addStyle={addStyle + "background: linear-gradient(90deg, rgba(225,0,0,1) 50%, rgba(0,138,0,1) 50%); background-clip: text; color: transparent;"}
+    {children}
+/>
