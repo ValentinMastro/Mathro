bce91834e36ed9ffbebb76d1ea30b16aa0dd8659
Valentin Mastro <valentinmastro@hotmail.fr>
2025-09-28 22:34:40 +0200

    On affiche les mesures des angles en les calculant avec le composant Angle et des store.

--- www/src/lib/cahier/contenu/5eme/04 - Angles dans un triangle/Page1.svelte
+++ www/src/lib/cahier/contenu/5eme/04 - Angles dans un triangle/Page1.svelte
@@ -1,50 +1,50 @@
 <script lang="ts">
-	import { Chapitre, Contenu, Partie } from "$lib/cahier/composants/de_chapitrage/*";
-	import { Exemples, Item, Propriete, Schema, Texte } from "$lib/cahier/composants/de_cours/*";
-	import LigneVide from "$lib/cahier/composants/LigneVide.svelte";
+	import { writable } from 'svelte/store';
+	import { Chapitre, Contenu, Partie } from '$lib/cahier/composants/de_chapitrage/*';
+	import { Exemples, Item, Propriete, Schema, Texte } from '$lib/cahier/composants/de_cours/*';
+	import { Angle, Polygone } from '$lib/cahier/composants/svg/*';
 
-	import { math } from "mathlifier";
+	import { math } from 'mathlifier';
+
+	let A = $state({ x: 200, y: 200 });
+	let B = $state({ x: 800, y: 300 });
+	let C = $state({ x: 500, y: 800 });
+
+	let ABC = writable(0);
+	let BCA = writable(0);
+	let CAB = writable(0);
+
+	$inspect(ABC);
 </script>
 
 <Chapitre titre="Angles d'un triangle" />
 
 <Contenu apres_un_titre>
 	<Partie numero={1} titre="La somme des angles d'un triangle" />
-		<LigneVide />
-		<Propriete lignes={2}>
-			Dans un triangle, la somme des mesures des trois angles est égale à 180°.
-		</Propriete>
-		<Exemples lignes={4}>
-			<Item lignes={2}>
-				Si, dans le triangle ABC, {@html math("\\widehat{\\text{ABC}} = 60^\\circ")} et {@html math("\\widehat{\\text{ACB}} = 40^\\circ")}, 
-				alors {@html math("\\widehat{\\text{BAC}} = 180 - 60 - 40 = 80^\\circ")}.
-			</Item>
-			<Item lignes={2}>
-				Si, dans le triangle DEF, {@html math("\\widehat{\\text{DEF}} = 90^\\circ")} et {@html math("\\widehat{\\text{DFE}} = 30^\\circ")},
-				alors {@html math("\\widehat{\\text{EDF}} = 180 - 90 - 30 = 60^\\circ")}.
-			</Item>
-		</Exemples>
-		<Schema lignes={10}>
-			{#snippet svg()}
-				<defs>
-					<polygon id="triangle" points="200,200 800,300 500,800" />
-					<style>
-						.angle {
-							font-size: 50px;
-						}
-					</style>
-				</defs>
-				<use href="#triangle" stroke="black" fill="none" stroke-width="8" />	
-				<text x={200} y={200} dx={40} dy={70} class="angle">53°</text>
-				<text x={800} y={300} dx={-100} dy={50} class="angle">67°</text>
-				<text x={500} y={800} dx={-30} dy={-70} class="angle">60°</text>
-			{/snippet}
-			{#snippet html()}
-				<LigneVide lignes={3} />
-				<Texte lignes={3}>
-					Après avoir mesuré les angles au rapporteur,
-					je remarque que {@html math("53^\\circ + 67^\\circ + 60^\\circ = 180^\\circ")}.
-				</Texte>
-			{/snippet}
-		</Schema>
+	<Propriete lignes={2}>Dans un triangle, la somme des mesures des trois angles est égale à 180°.</Propriete>
+	<Exemples lignes={4}>
+		<Item lignes={2}>
+			Si, dans le triangle ABC, {@html math('\\widehat{\\text{ABC}} = 60^\\circ')} et {@html math('\\widehat{\\text{ACB}} = 40^\\circ')}, <br />
+			alors {@html math('\\widehat{\\text{BAC}} = 180 - 60 - 40 = 80^\\circ')}.
+		</Item>
+		<Item lignes={2}>
+			Si, dans le triangle DEF, {@html math('\\widehat{\\text{DEF}} = 90^\\circ')} et {@html math('\\widehat{\\text{DFE}} = 30^\\circ')}, <br />
+			alors {@html math('\\widehat{\\text{EDF}} = 180 - 90 - 30 = 60^\\circ')}.
+		</Item>
+	</Exemples>
+	<Schema lignes={10} html_lignes_vides={3}>
+		{#snippet svg()}
+			<Angle r={70} points={[A, B, C]} fill="red" afficher_mesure mesure={ABC} />
+			<Angle r={70} points={[B, C, A]} fill="blue" afficher_mesure mesure={BCA} />
+			<Angle r={70} points={[C, A, B]} fill="green" afficher_mesure mesure={CAB} />
+			<Polygone points={[A, B, C]} afficher_points />
+		{/snippet}
+		{#snippet html()}
+			<Texte lignes={3}>
+				Après avoir mesuré les angles au <br />
+				rapporteur, je remarque que <br />
+				{@html math(`${$ABC.toFixed()}^\\circ + ${$BCA.toFixed()}^\\circ + ${$CAB.toFixed()}^\\circ = ${$ABC + $BCA + $CAB}^\\circ`)}.
+			</Texte>
+		{/snippet}
+	</Schema>
 </Contenu>
