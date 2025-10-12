6dddb1e1a971955ddb45dcfa2ef16946c7d38711
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-13 01:05:36 +0200

    Question

---
+++ www/src/lib/cahier/composants/d_exercices/Question.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+	let { numéro = 1, lignes = 1, children }: { numéro: number; lignes?: number; children?: Snippet } = $props();
+</script>
+
+<div class="question" style="--lignes: {lignes};">
+	Q{numéro}) {@render children?.()}
+</div>
+
+<style>
+	.question {
+		font-size: var(--font-size);
+		line-height: calc(var(--line-height) * 1.5);
+		height: calc(var(--lignes) * var(--carreau));
+		width: 100%;
+	}
+</style>
