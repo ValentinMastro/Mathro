c41bb7e1a7487d63ae7e1b192e7803eab439b923
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-12 18:36:26 +0200

    4èmes ch. 4 et 5

---
+++ www/src/lib/cahier/contenu/4eme/04 - Transformations du plan/Page4.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Contenu, Partie } from '$lib/cahier/composants/de_chapitrage/*';
+	import { Propriete, Item, Texte } from '$lib/cahier/composants/de_cours/*';
+</script>
+
+<Contenu>
+	<Partie numero={4} titre="Propriété de conservation" />
+	<Propriete lignes={2}>
+		La symétrie axiale, la symétrie centrale et la translation sont des <i>isométries du plan</i>, ce qui signifie que :
+	</Propriete>
+	<Item>la nature des objets est conservée</Item>
+	<Texte couleur="bleu">(Le symétrique d'un carré est un carré.)</Texte>
+	<Item>les longueurs sont conservées</Item>
+	<Texte couleur="bleu" lignes={2}>(le symétrique d'un segment de longueur 5 cm est un segment de longueur 5 cm)</Texte>
+	<Item>les mesures des angles sont conservées</Item>
+	<Texte couleur="bleu">(le symétrique d'un angle de 90° est un angle de 90°)</Texte>
+	<Item>les aires sont conservées</Item>
+	<Texte couleur="bleu" lignes={2}>(le symétrique d'une figure d'aire de 10 cm² est aussi une figure d'aire de 10 cm²)</Texte>
+</Contenu>
---
+++ www/src/lib/cahier/contenu/4eme/05 - Calcul littéral/Page5.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { Contenu, Partie, SousPartie } from '$lib/cahier/composants/de_chapitrage/*';
+	import { Definition, Exemples, Item, Paragraphe } from '$lib/cahier/composants/de_cours/*';
+	import { LatexAlign } from '$lib/cahier/composants/math/*';
+	import { math } from 'mathlifier';
+</script>
+
+<Contenu>
+	<Partie numero={2} titre="Développer, factoriser, réduire" />
+	<Definition lignes={4}>
+		Développer une expression littérale, c'est transformer un produit en une somme.<br />
+		Factoriser une expression littérale, c'est transformer une somme en un produit.
+	</Definition>
+	<SousPartie numero={1} titre="Distributivité" />
+	<Paragraphe couleur="rouge" nom_du_paragraphe="Règle">
+		Soient {@html math('a')}, {@html math('b')} et {@html math('k')} trois nombres :
+		{@html math('k (a + b) = ka + kb')}
+	</Paragraphe>
+	<Exemples lignes={11}>
+		<LatexAlign
+			lignes={2}
+			code="
+			    x(x+4)  &= x \times x + x \times 4 \\
+                        &= x^2 + 4x"
+		/>
+		<LatexAlign
+			lignes={2}
+			code="
+			    3(2x+5) &= 3 \times 2x + 3 \times 5 \\
+                        &= 6x + 15"
+		/>
+		<LatexAlign
+			lignes={2}
+			code="
+			    2x(x+3) &= 2x \times x + 2x \times 3 \\
+                        &= 2x^2 + 6x"
+		/>
+		<LatexAlign
+			lignes={2}
+			code="
+			    2x + xy &= 2 \fbox{'{$x$}'} + \fbox{'{$x$}'} y \\
+                        &= x (2 + y)"
+		/>
+		<LatexAlign
+			lignes={3}
+			code="
+                3x + 15 &= 3x + 3 \times 5 \\
+                        &= \fbox{'{$3$}'} x + \fbox{'{$3$}'} \times 5 \\
+                        &= 3 (x + 5)"
+		/>
+	</Exemples>
+</Contenu>
