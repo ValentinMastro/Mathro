8ed015101bc47db3f737028bd56dd1997fc046c6
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-12 20:57:57 +0200

    Déplacement des pages de cours

---
+++ www/src/lib/cahier/exercices/4eme/03 - Fractions et proportionnalité/Page1.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Chapitre, Contenu } from '$lib/cahier/composants/de_chapitrage/*';
+</script>
+
+<Chapitre titre="Fractions et proportionnalité" />
+
+<Contenu></Contenu>
