da49ae5b3e0092bfd350b5dfcd75c2c07369dea7
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-13 01:04:55 +0200

    Composant de marge : ACtivité

--- www/src/lib/cahier/composants/de_marge/*.ts
+++ www/src/lib/cahier/composants/de_marge/*.ts
@@ -1,3 +1,4 @@
+export { default as Activité } from './Activité.svelte';
 export { default as Exercice } from './Exercice.svelte';
 export { default as Nombre } from './Nombre.svelte';
 export { default as Slider } from './Slider.svelte';
---
+++ www/src/lib/cahier/composants/de_marge/Activité.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import LigneVide from '$lib/cahier/composants/LigneVide.svelte';
+
+	interface Props {
+		lignes_vides?: number;
+		numéro: number;
+	}
+
+	let { numéro, lignes_vides = 1 }: Props = $props();
+</script>
+
+<div class="activité">
+	<center>
+		<span>
+			Activité {numéro}
+		</span>
+	</center>
+</div>
+<LigneVide lignes={lignes_vides} />
+
+<style>
+	.activité {
+		width: 100%;
+		margin-left: auto;
+		margin-right: auto;
+		color: var(--noir);
+		position: relative;
+		height: var(--carreau);
+	}
+	span {
+		width: fit-content;
+		font-size: calc(var(--taille-page) / 50);
+		line-height: calc(var(--taille-page) / 50 * 1.9);
+	}
+</style>
