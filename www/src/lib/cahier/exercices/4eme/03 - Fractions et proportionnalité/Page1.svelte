4cc07542c2206586d80e2bca21a60c9ffe65b06a
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-13 04:17:35 +0200

    Activités

--- www/src/lib/cahier/exercices/4eme/03 - Fractions et proportionnalité/Page1.svelte
+++ www/src/lib/cahier/exercices/4eme/03 - Fractions et proportionnalité/Page1.svelte
@@ -1,7 +1,35 @@
 <script lang="ts">
-	import { Chapitre, Contenu } from '$lib/cahier/composants/de_chapitrage/*';
+	import { Question, Énoncé } from '$lib/cahier/composants/d_exercices/*';
+	import { Chapitre, Contenu, DansLaMarge } from '$lib/cahier/composants/de_chapitrage/*';
+	import { Texte } from '$lib/cahier/composants/de_cours/*';
+	import { Activité } from '$lib/cahier/composants/de_marge/*';
+	import { math } from 'mathlifier';
 </script>
 
 <Chapitre titre="Fractions et proportionnalité" />
 
-<Contenu></Contenu>
+<Activité numéro={1} position={3} />
+
+<Contenu apres_un_titre>
+	<Énoncé>
+		<Texte lignes={8}>
+			Sur une &laquo; fiche de paie &raquo;, sont affichées les informations suivantes : <br />
+			&emsp; &starf; Coût total pour l'employeur (super brut) : 5033,52 € <br />
+			&emsp; &starf; Salaire brut : 2841,90 € <br />
+			&emsp; &starf; Salaire net à payer avant impôts sur le revenu : 2227,45 € <br />
+			&emsp; &starf; Salaire net à payer (super net) : 2093,83 € <br />
+			&emsp; &starf; Contribution patronale PC (pension civile) : 1795,75 € <br />
+			&emsp; &starf; Retenue salariale PC (pension civile) : 254,64 € <br />
+			&emsp; &starf; C.S.G : 66,25 € + 187,70 € <br />
+		</Texte>
+
+		<Question numéro={1} lignes={2}>Quel pourcentage du salaire super brut représente le salaire super net ?</Question>
+		<Question numéro={2} lignes={2}>
+			Combien d'€ sont reversés pour les pensions ? Quel pourcentage du salaire super brut représente cette somme ?
+		</Question>
+		<Question numéro={3} lignes={2}>Vérifier que la C.S.G correspond à 9,2 % de (98,25 % du salaire brut - 32,45 €).</Question>
+		<Question numéro={4} lignes={2}>Vérifier que le taux de prélèvement à la source de l'impôt sur le revenu est <b>d'environ</b> 5,70 %.</Question>
+		<Question numéro={5} lignes={2}>Vérifier que la C.R.D.S, égal à 0,5 % de (98,25 % du salaire brut - 32,45 €), est de 13,80 €.</Question>
+		<Question numéro={6} lignes={2}>On dit parfois que &laquo; le salaire net est 80% du salaire brut &raquo;. Qu'en penser ici ?</Question>
+	</Énoncé>
+</Contenu>
---
+++ www/src/lib/cahier/exercices/4eme/03 - Fractions et proportionnalité/Page2.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { Question, Énoncé } from '$lib/cahier/composants/d_exercices/*';
+	import { Contenu, DansLaMarge } from '$lib/cahier/composants/de_chapitrage/*';
+	import { CelluleTableau, Tableau, Texte } from '$lib/cahier/composants/de_cours/*';
+	import LigneTableau from '$lib/cahier/composants/de_cours/LigneTableau.svelte';
+	import { Activité } from '$lib/cahier/composants/de_marge/*';
+</script>
+
+<Activité numéro={2} />
+
+<Contenu>
+	<Énoncé>
+		<Texte>Voici le barême de l'impôt sur le revenu de 2025 :</Texte>
+		<Tableau colonnes={20} lignes={6}>
+			<LigneTableau>
+				<CelluleTableau colonnes={10}>Tranche (revenu annuel)</CelluleTableau>
+				<CelluleTableau colonnes={10}>Taux d'imposition</CelluleTableau>
+			</LigneTableau>
+			<LigneTableau>
+				<CelluleTableau colonnes={10}>Jusqu'à 11 497 €</CelluleTableau>
+				<CelluleTableau colonnes={10}>0%</CelluleTableau>
+			</LigneTableau>
+			<LigneTableau>
+				<CelluleTableau colonnes={10}>De 11 498 € à 29 315 €</CelluleTableau>
+				<CelluleTableau colonnes={10}>11%</CelluleTableau>
+			</LigneTableau>
+			<LigneTableau>
+				<CelluleTableau colonnes={10}>De 29 316 € à 83 823 €</CelluleTableau>
+				<CelluleTableau colonnes={10}>30%</CelluleTableau>
+			</LigneTableau>
+			<LigneTableau>
+				<CelluleTableau colonnes={10}>De 83 824 € à 180 294 €</CelluleTableau>
+				<CelluleTableau colonnes={10}>41%</CelluleTableau>
+			</LigneTableau>
+			<LigneTableau>
+				<CelluleTableau colonnes={10}>Supérieur à 180 294 €</CelluleTableau>
+				<CelluleTableau colonnes={10}>45%</CelluleTableau>
+			</LigneTableau>
+		</Tableau>
+		<Texte lignes={7}>
+			Chaque foyer fiscal a un <i>nombre de parts</i>, qui divide le revenu annuel avant l'application des taux d'imposition.<br />
+			Le nombre de parts est calculé en fonction du nombre d'adultes et d'enfants dans le foyer fiscal : <br />
+			&emsp; &starf; 1 par adulte <br />
+			&emsp; &starf; 0,5 par enfant (1er et 2e), 1 par enfant (3e et suivants)
+		</Texte>
+		<Question numéro={1} lignes={2}>Si un couple a 3 enfants, quel est le nombre de parts du foyer fiscal ?</Question>
+		<Question numéro={2} lignes={2}>Si une personne a un revenu annuel de 11 497 €, quel est son revenu mensuel moyen ?</Question>
+		<Question numéro={3} lignes={2}>
+			Calculer l'impôt sur le revenu pour un célibataire sans enfant (1 part) ayant un revenu annuel de 35 000 €.
+		</Question>
+	</Énoncé>
+</Contenu>
