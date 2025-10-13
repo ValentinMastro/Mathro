294e5056384d7c23b2740e41aa0804288b6ffec5
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-13 04:19:18 +0200

    Activité n'est plus programmé comme dans la marge

--- www/src/lib/cahier/composants/de_marge/Activité.svelte
+++ www/src/lib/cahier/composants/de_marge/Activité.svelte
@@ -4,32 +4,37 @@
 	interface Props {
 		lignes_vides?: number;
 		numéro: number;
+		position?: number;
 	}
 
-	let { numéro, lignes_vides = 1 }: Props = $props();
+	let { numéro, lignes_vides = 1, position = 1 }: Props = $props();
 </script>
 
-<div class="activité">
-	<center>
-		<span>
-			Activité {numéro}
-		</span>
-	</center>
+<div class="activité" style="--position: {position};">
+	<span>
+		Activité {numéro}
+	</span>
 </div>
 <LigneVide lignes={lignes_vides} />
 
 <style>
 	.activité {
-		width: 100%;
-		margin-left: auto;
-		margin-right: auto;
-		color: var(--noir);
-		position: relative;
+		position: absolute;
+		top: calc(8.72% + (var(--position) - 1) * var(--carreau));
+
+		width: 20%;
 		height: var(--carreau);
-	}
-	span {
-		width: fit-content;
-		font-size: calc(var(--taille-page) / 50);
-		line-height: calc(var(--taille-page) / 50 * 1.9);
+		color: var(--noir);
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-end;
+
+		span {
+			margin-bottom: -0.1em;
+			font-size: calc(var(--taille-page) / 50);
+			line-height: calc(var(--taille-page) / 50);
+		}
 	}
 </style>
