700d6007bce377528d35227b239dfb46c99c9336
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-13 04:17:46 +0200

    Composant Énoncé

---
+++ www/src/lib/cahier/composants/d_exercices/Énoncé.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+	let { lignes = 1, children }: { lignes?: number; children?: Snippet } = $props();
+</script>
+
+<div class="énoncé" style="--lignes: {lignes};">
+	{@render children?.()}
+</div>
+
+<style>
+	.énoncé {
+		font-size: var(--font-size);
+		line-height: var(--line-height);
+		height: calc(var(--lignes) * var(--carreau));
+		width: 100%;
+	}
+</style>
