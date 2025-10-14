7b87d13773b7b21291f3ee2f0da8255bd71d6dc7
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-14 14:01:19 +0200

    Suppression du nombre rationnel

--- www/src/lib/cahier/cours/5eme/01 - Fractions/Page1.svelte
+++ www/src/lib/cahier/cours/5eme/01 - Fractions/Page1.svelte
@@ -17,30 +17,14 @@
 	<Notation lignes={1.3}>
 		On le note {@html math('\\dfrac{a}{b}')}.
 	</Notation>
-	<Exemples lignes={5}>
-		<Item lignes={2}>
-			{@html math('\\dfrac{7}{3}')} est le quotient de 7 par 3.<br />
-			{@html math('\\dfrac{7}{3} \\times 3 = \\dfrac{7}{\\cancel{3}} \\times \\cancel{3} = 7')}
-		</Item>
-		<LigneVide />
-		<Item lignes={2}>
-			{@html math('\\dfrac{17}{6}')} est le quotient de 17 par 6. <br />
-			{@html math('\\dfrac{17}{6} \\times 6 = \\dfrac{17}{\\cancel{6}} \\times \\cancel{6} = 17')}
-		</Item>
-	</Exemples>
-	<Vocabulaire lignes={2}>On dit qu'un nombre est rationnel lorsqu'il peut s'écrire sous forme de fraction.</Vocabulaire>
 	<Exemples lignes={7}>
-		<Item>
-			{@html math('\\frac{3}{4}')} est rationnel car c'est une fraction.
+		<Item lignes={4}>
+			{@html math('\\dfrac{7}{3}')} est le quotient de 7 par 3.<br /><br />
+			{@html math('\\dfrac{7}{3} \\times 3 = \\dfrac{7}{\\cancel{3}} \\times \\cancel{3} = 7')}<br />
 		</Item>
-		<Item lignes={2}>
-			{@html math('1{,}5')} est un nombre rationnel car il peut s'écrire sous forme de fraction : {@html math('1{,}5 = \\frac{3}{2}')}
-		</Item>
-		<Item lignes={2}>
-			{@html math('1{,}333...')} est rationnel car il peut s'écrire sous forme de fraction : {@html math('1{,}333... = \\frac{4}{3}')}
-		</Item>
-		<Item lignes={2}>
-			{@html math('\\pi')} n'est pas un nombre rationnel, il n'est pas possible de l'écrire sous forme de fraction.
+		<Item lignes={3}>
+			{@html math('\\dfrac{17}{6}')} est le quotient de 17 par 6. <br /> <br />
+			{@html math('\\dfrac{17}{6} \\times 6 = \\dfrac{17}{\\cancel{6}} \\times \\cancel{6} = 17')}
 		</Item>
 	</Exemples>
 </Contenu>
