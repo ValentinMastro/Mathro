ed6f6e67a2a1892b49168781d9ad4f4f1ef55104
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-15 07:24:31 +0200

    Exercice.svelte

---
+++ www/src/lib/cahier/composants/d_exercices/Exercice.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { Paragraphe, type ParagrapheProps } from '$lib/cahier/composants/de_cours/*';
+	let { addStyle, nom_du_paragraphe = 'Exercice :', couleur = 'noir', lignes, lignes_vides, children }: ParagrapheProps = $props();
+</script>
+
+<Paragraphe {addStyle} {nom_du_paragraphe} {couleur} {lignes} {lignes_vides} {children} />
