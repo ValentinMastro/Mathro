81ce9f2cda335710a6e449d89e73b0dece9957c8
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-21 00:28:55 +0200

    Utilisation des nouveaux composants, et du type NombreDecimal

--- www/src/lib/cahier/cours/6eme/03 - Nombres décimaux/Page1.svelte
+++ www/src/lib/cahier/cours/6eme/03 - Nombres décimaux/Page1.svelte
@@ -1,32 +1,25 @@
 <script lang="ts">
 	import { Chapitre, Contenu, DansLaMarge, Partie } from '$lib/cahier/composants/de_chapitrage/*';
 	import { Tableau, LigneTableau, Exemples, Item, CelluleTableau } from '$lib/cahier/composants/de_cours/*';
-	import { Nombre } from '$lib/cahier/composants/de_marge/*';
-	import LigneVide from '$lib/cahier/composants/LigneVide.svelte';
+	import { SaisieNombreDecimal, Exercice } from '$lib/cahier/composants/de_marge/*';
+	import { NombreDecimal, POSITIONS } from '$lib/cahier/composants/math/*';
 
-	let nombre_1 = $state(123.456_789);
-	let nombre_2 = $state(13.482);
-	let nombre_3 = $state(0.7894);
+	let _nombre_1 = $state(NombreDecimal.depuisPartiesEntieresEtDecimales(123n, 456789n, 'POSITIF'));
+	let _nombre_2 = $state(NombreDecimal.depuisPartiesEntieresEtDecimales(13n, 482n, 'POSITIF'));
+	let _nombre_3 = $state(NombreDecimal.depuisPartiesEntieresEtDecimales(0n, 7894n, 'POSITIF'));
 
-	function chiffre(nombre: number, puissance: number | undefined) {
-		if (puissance == undefined) {
-			return ',';
-		}
-		return Math.floor(nombre / Math.pow(10, puissance)) % 10;
-	}
-
-	function nombre_de(nombre: number, puissance: number) {
-		return Math.floor(nombre / Math.pow(10, puissance));
-	}
+	let min = NombreDecimal.depuisEntier(0n);
+	let max = NombreDecimal.depuisEntier(999n);
 </script>
 
 <Chapitre titre="Nombres décimaux" />
 
-<DansLaMarge>
-	<LigneVide lignes={19} />
-	<Nombre bind:valeur={nombre_1} min={0} max={999} step={0.000_001} />
-	<Nombre bind:valeur={nombre_2} min={0} max={999} step={0.000_001} />
-	<Nombre bind:valeur={nombre_3} min={0} max={999} step={0.000_001} />
+<DansLaMarge lignes_vides={19}>
+	<SaisieNombreDecimal bind:value={_nombre_1} {min} {max} step={new NombreDecimal(1n, 6n, 'POSITIF')} />
+	<SaisieNombreDecimal bind:value={_nombre_2} {min} {max} step={new NombreDecimal(1n, 3n, 'POSITIF')} />
+	<SaisieNombreDecimal bind:value={_nombre_3} {min} {max} step={new NombreDecimal(1n, 4n, 'POSITIF')} />
+	<Exercice niveau={6} id_exercice={1} position={23} />
+	<Exercice niveau={6} id_exercice={2} position={26} />
 </DansLaMarge>
 
 <Contenu apres_un_titre>
@@ -42,17 +35,23 @@
 				<CelluleTableau
 					colonnes={2}
 					texte_vertical
-					addStyle="text-transform: uppercase; padding-top: calc(0.5*var(--carreau)); letter-spacing: 0.27em;"
+					addStyle="text-transform: uppercase; padding-top: calc(0.25*var(--carreau)); letter-spacing: 0.26em;"
 				>
 					{t}
 				</CelluleTableau>
 			{/each}
 		</LigneTableau>
-		{#each [nombre_1, nombre_2, nombre_3] as nombre}
+		{#each [_nombre_1, _nombre_2, _nombre_3] as nombre}
 			<LigneTableau>
-				{#each [2, 1, 0, undefined, -1, -2, -3, -4, -5, -6] as puissance}
+				{#each ['centaines', 'dizaines', 'unités', undefined, 'dixièmes', 'centièmes', 'millièmes', 'dix-millièmes', 'cent-millièmes', 'millionièmes'] as position}
 					<CelluleTableau colonnes={2}>
-						{chiffre(nombre, puissance)}
+						{#if position == 'unités' && nombre.inférieur_à(1)}
+							0
+						{:else if position}
+							{nombre.chiffre_significatif_des(position as keyof typeof POSITIONS)}
+						{:else}
+							,
+						{/if}
 					</CelluleTableau>
 				{/each}
 			</LigneTableau>
@@ -61,14 +60,14 @@
 
 	<Exemples lignes={6}>
 		<Item lignes={3}>
-			{nombre_2.toLocaleString('fr-FR')} :
-			<Item>le chiffre des unités est {chiffre(nombre_2, 0)}</Item>
-			<Item>le nombre des dixièmes est {nombre_de(nombre_2, -1)}</Item>
+			{_nombre_2} :
+			<Item>le chiffre des unités est {_nombre_2.chiffre_significatif_des('unités')}</Item>
+			<Item>le nombre des dixièmes est {_nombre_2.nombres_des('dixièmes')}</Item>
 		</Item>
 		<Item lignes={3}>
-			{nombre_3.toLocaleString('fr-FR', { minimumFractionDigits: 4, maximumFractionDigits: 6 })} :
-			<Item>le chiffre des dix-millièmes est {chiffre(nombre_3, -4)}</Item>
-			<Item>le nombre de dix-millièmes est {nombre_de(nombre_3, -4)}</Item>
+			{_nombre_3} :
+			<Item>le chiffre des dix-millièmes est {_nombre_3.chiffre_significatif_des('dix-millièmes')}</Item>
+			<Item>le nombre de dix-millièmes est {_nombre_3.nombres_des('dix-millièmes')}</Item>
 		</Item>
 	</Exemples>
 </Contenu>
