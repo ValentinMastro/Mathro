104dbe0c4ace620c9e6597420c6e269c102d494c
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-21 00:26:46 +0200

    Composant de saisie de nombre

--- www/src/lib/cahier/composants/de_marge/*.ts
+++ www/src/lib/cahier/composants/de_marge/*.ts
@@ -1,3 +1,5 @@
 export { default as Exercice } from './Exercice.svelte';
 export { default as Nombre } from './Nombre.svelte';
+export { default as SaisieNombreDecimal } from './SaisieNombreDecimal.svelte';
+export { default as SaisieNombreEntier } from './SaisieNombreEntier.svelte';
 export { default as Slider } from './Slider.svelte';
