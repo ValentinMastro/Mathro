e70035b5d1e099e8cf7387a06566110416c90090
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-21 00:27:53 +0200

    On utilise les noouveaux composants liés au bigint

--- www/src/lib/cahier/cours/6eme/01 - Fractions/Page1.svelte
+++ www/src/lib/cahier/cours/6eme/01 - Fractions/Page1.svelte
@@ -1,32 +1,19 @@
 <script lang="ts">
 	import { Chapitre, Partie, SousPartie, Contenu, DansLaMarge } from '$lib/cahier/composants/de_chapitrage/*';
-	import { Tableau, LigneTableau, Texte, Exemples, Item, Definitions, PourMieuxComprendre, CelluleTableau } from '$lib/cahier/composants/de_cours/*';
-	import { Exercice, Nombre } from '$lib/cahier/composants/de_marge/*';
-	import LigneVide from '$lib/cahier/composants/LigneVide.svelte';
+	import { Tableau, LigneTableau, Exemples, Item, Definitions, PourMieuxComprendre, CelluleTableau } from '$lib/cahier/composants/de_cours/*';
+	import { Exercice, SaisieNombreEntier } from '$lib/cahier/composants/de_marge/*';
+	import { NombreDecimal } from '$lib/cahier/composants/math/*';
 
-	let numero_1 = $state(1234);
-	let numero_2 = $state(1500036000);
+	let _numero_1 = $state(1234n);
+	let _numero_2 = $state(1500036000n);
 	let surbrillances = $state([false, false, false, false]);
-
-	function chiffre(n: number, puissance: number): number {
-		return Math.floor(n / 10 ** puissance) % 10;
-	}
-
-	function chiffre_sans_zeros_inutiles(n: number, puissance: number): number | string {
-		if (10 ** puissance > n) return '';
-		return chiffre(n, puissance);
-	}
-
-	function nombre(n: number, puissance: number): number {
-		return Math.floor(n / 10 ** puissance);
-	}
 </script>
 
 <Chapitre titre="Fractions" />
 
-<DansLaMarge apres_un_titre={true} lignes_vides={15}>
-	<Nombre bind:valeur={numero_1} min={1000} max={9999} />
-	<Nombre bind:valeur={numero_2} min={1000000000} max={9999999999} />
+<DansLaMarge apres_un_titre lignes_vides={15}>
+	<SaisieNombreEntier bind:value={_numero_1} min={1000n} max={9999n} />
+	<SaisieNombreEntier bind:value={_numero_2} min={1000000000n} max={9999999999n} />
 	<Exercice niveau={6} id_exercice={1} position={20} />
 	<Exercice niveau={6} id_exercice={2} position={23} />
 </DansLaMarge>
@@ -36,20 +23,22 @@
 	puissance_chiffre: number,
 	index_surbrillance_nombre: number,
 	puissance_nombre: number,
-	numero: number
+	numero: bigint
 )}
+	{@const nombre = NombreDecimal.depuisEntier(numero)}
 	{#each Array(12) as _, index}
 		{@const condition =
 			(surbrillances[index_surbrillance_chiffre] && index == 11 - puissance_chiffre) ||
 			(surbrillances[index_surbrillance_nombre] && index <= 11 - puissance_nombre)}
 		<CelluleTableau addStyle="border-right: 1px solid black; {condition ? 'background-color: red; color: white;' : ''}">
-			{chiffre_sans_zeros_inutiles(numero, 11 - index)}
+			{nombre.chiffre_significatif_des(11 - index)}
 		</CelluleTableau>
 	{/each}
 {/snippet}
-{#snippet ligne_de_texte(index_surbrillance: number, numero: number, puissance: number, texte: string)}
+{#snippet ligne_de_texte(index_surbrillance: number, numero: bigint, puissance: number, texte: string)}
+	{@const nombre = NombreDecimal.depuisEntier(numero)}
 	<Item>
-		{chiffre(numero, puissance)} est
+		{nombre.chiffre_des(puissance)} est
 		<span
 			role="tooltip"
 			onmouseenter={() => (surbrillances[index_surbrillance] = true)}
@@ -60,9 +49,10 @@
 		de {numero.toLocaleString()}.
 	</Item>
 {/snippet}
-{#snippet ligne_de_texte_nombre(index_surbrillance: number, numero: number, puissance: number, texte: string)}
+{#snippet ligne_de_texte_nombre(index_surbrillance: number, numero: bigint, puissance: number, texte: string)}
+	{@const nombre = NombreDecimal.depuisEntier(numero)}
 	<Item>
-		{nombre(numero, puissance)} est
+		{nombre.nombres_des(puissance)} est
 		<span
 			role="tooltip"
 			onmouseenter={() => (surbrillances[index_surbrillance] = true)}
@@ -99,16 +89,15 @@
 				<CelluleTableau addStyle="border-right: 1px solid black;">U</CelluleTableau>
 			{/each}
 		</LigneTableau>
-		<LigneTableau>{@render ligne_de_chiffres(0, 2, 2, 1, numero_1)}</LigneTableau>
-		<LigneTableau>{@render ligne_de_chiffres(1, 4, 3, 4, numero_2)}</LigneTableau>
+		<LigneTableau>{@render ligne_de_chiffres(0, 2, 2, 1, _numero_1)}</LigneTableau>
+		<LigneTableau>{@render ligne_de_chiffres(1, 4, 3, 4, _numero_2)}</LigneTableau>
 	</Tableau>
-	<LigneVide />
 
 	<SousPartie numero={2} titre="Chiffres de ... / Nombres de ..." />
 	<Exemples lignes={4}>
-		{@render ligne_de_texte(0, numero_1, 2, 'le chiffre des centaines')}
-		{@render ligne_de_texte(1, numero_2, 4, 'le chiffre des dizaines de milliers')}
-		{@render ligne_de_texte_nombre(2, numero_1, 1, 'le nombre des dizaines')}
-		{@render ligne_de_texte_nombre(3, numero_2, 4, 'le nombre des dizaines de milliers')}
+		{@render ligne_de_texte(0, _numero_1, 2, 'le chiffre des centaines')}
+		{@render ligne_de_texte(1, _numero_2, 4, 'le chiffre des dizaines de milliers')}
+		{@render ligne_de_texte_nombre(2, _numero_1, 1, 'le nombre des dizaines')}
+		{@render ligne_de_texte_nombre(3, _numero_2, 4, 'le nombre des dizaines de milliers')}
 	</Exemples>
 </Contenu>
