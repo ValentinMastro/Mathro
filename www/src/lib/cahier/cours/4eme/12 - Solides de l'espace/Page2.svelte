9556a02a17c6879da80c5d421fa881f4b5d87031
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-21 00:29:12 +0200

    Nouveaux composants de Tableau

--- www/src/lib/cahier/cours/4eme/12 - Solides de l'espace/Page2.svelte
+++ www/src/lib/cahier/cours/4eme/12 - Solides de l'espace/Page2.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Contenu, Partie } from '$lib/cahier/composants/de_chapitrage/*';
-	import { Definition, Convention, Tableau, LigneTableau } from '$lib/cahier/composants/de_cours/*';
+	import { Definition, Convention, Tableau, LigneTableau, Texte, CelluleTableau } from '$lib/cahier/composants/de_cours/*';
 	import { math } from 'mathlifier';
 </script>
 
@@ -10,40 +10,42 @@
 		Le volume d'un solide est la place qu'il occupe dans l'espace.<br />
 		La contenance d'un récipient est le volume de liquide qu'il peut contenir.
 	</Definition>
-	<Convention lignes={3}>
+	<Convention lignes={3} lignes_vides={2}>
 		Dans le Système International d'unités (SI), le volume s'exprime en mètres cubes ({@html math('\\text{m}^3')}).<br />
 		On exprime la contenance en litres ({@html math('\\ell')}), sachant que {@html math('1000 \\ell = 1~\\text{m}^3')}.
 	</Convention>
 
-	<Tableau lignes={3}>
-		<caption>Tableau de conversion des unités de volume et de contenance</caption>
+	<Texte couleur="noir">
+		<center>
+			<u>Tableau de conversion des unités de volume et de contenance</u>
+		</center>
+	</Texte>
+	<Tableau lignes={3} colonnes={21}>
 		<LigneTableau>
 			{#each Array(11) as _}
-				<th></th>
+				<CelluleTableau />
 			{/each}
-			<th>k&#x2113;</th>
-			<th>h&#x2113;</th>
-			<th>da&#x2113;</th>
-			<th>&#x2113;</th>
-			<th>d&#x2113;</th>
-			<th>c&#x2113;</th>
-			<th>m&#x2113;</th>
-			<th></th>
-			<th></th>
-			<th></th>
+			<CelluleTableau>{@html math('\\text{k}\\ell')}</CelluleTableau>
+			<CelluleTableau>{@html math('\\text{h}\\ell')}</CelluleTableau>
+			<CelluleTableau>{@html math('\\text{da}\\ell')}</CelluleTableau>
+			<CelluleTableau>{@html math('\\ell')}</CelluleTableau>
+			<CelluleTableau>{@html math('\\text{d}\\ell')}</CelluleTableau>
+			<CelluleTableau>{@html math('\\text{c}\\ell')}</CelluleTableau>
+			<CelluleTableau>{@html math('\\text{m}\\ell')}</CelluleTableau>
+			{#each Array(3) as _}
+				<CelluleTableau />
+			{/each}
 		</LigneTableau>
 		<LigneTableau>
-			<th colspan="3">km<sup>3</sup></th>
-			<th colspan="3">hm<sup>3</sup></th>
-			<th colspan="3">dam<sup>3</sup></th>
-			<th colspan="3">m<sup>3</sup></th>
-			<th colspan="3">dm<sup>3</sup></th>
-			<th colspan="3">cm<sup>3</sup></th>
-			<th colspan="3">mm<sup>3</sup></th>
+			{#each ['\\text{km}^3', '\\text{hm}^3', '\\text{da}m^3', '\\text{m}^3', '\\text{dm}^3', '\\text{cm}^3', '\\text{mm}^3'] as unite}
+				<CelluleTableau colonnes={3}>
+					{@html math(unite)}
+				</CelluleTableau>
+			{/each}
 		</LigneTableau>
-		<LigneTableau>
+		<LigneTableau lignes={5}>
 			{#each Array(21) as _}
-				<td></td>
+				<CelluleTableau />
 			{/each}
 		</LigneTableau>
 	</Tableau>
