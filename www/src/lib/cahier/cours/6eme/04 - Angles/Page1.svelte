6c022469705f70dfc6e39ad1697eee214ae0e053
Valentin Mastro <valentinmastro@hotmail.fr>
2025-10-21 15:59:22 +0200

    Utilisation des composants svg

--- www/src/lib/cahier/cours/6eme/04 - Angles/Page1.svelte
+++ www/src/lib/cahier/cours/6eme/04 - Angles/Page1.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import { Chapitre, Contenu, Partie } from '$lib/cahier/composants/de_chapitrage/*';
-	import { Definition, Item, LigneTableau, Notation, Schema, Tableau } from '$lib/cahier/composants/de_cours/*';
-	import LigneVide from '$lib/cahier/composants/LigneVide.svelte';
-
+	import { Chapitre, Contenu } from '$lib/cahier/composants/de_chapitrage/*';
+	import { Definition, Item, Notation, Schema, Texte } from '$lib/cahier/composants/de_cours/*';
+	import { Angle, DemiDroite, Point } from '$lib/cahier/composants/svg/*';
 	import { math } from 'mathlifier';
+
+	const A = { x: 200, y: 800 };
+	const B = { x: 490, y: 300 };
+	const C = { x: 800, y: 800 };
 </script>
 
 <Chapitre titre="Angles" />
@@ -19,29 +22,27 @@
 			ses deux <i>côtés</i> : les deux demi-droites,
 		</Item>
 	</Definition>
-	<Schema lignes={5}>
+	<Schema lignes={10} aspectRatioSVG={2}>
 		{#snippet svg()}
-			<circle cx={200} cy={800} r={5} fill="black" />
-			<line x1={200} y1={800} x2={200 + 800 * Math.cos(0)} y2={800 + 800 * Math.sin(0)} stroke="black" stroke-width={5} />
-			<line x1={200} y1={800} x2={200 + 800 * Math.cos(-Math.PI / 3)} y2={800 + 800 * Math.sin(-Math.PI / 3)} stroke="black" stroke-width={5} />
-			<line x1={450} x2={450 + 100} y1={300} y2={300} stroke="black" stroke-width={5} />
-			<line x1={800} x2={800} y1={750} y2={850} stroke="black" stroke-width={5} />
-			<text x={200} y={800} dx={-70} dy={0} font-size={90} fill="black">A</text>
-			<text x={450} y={300} dx={0} dy={-50} font-size={90} fill="black">B</text>
-			<text x={800} y={750} dx={0} dy={-50} font-size={90} fill="black">C</text>
-		{/snippet}
-		{#snippet html()}
-			<Item>
-				L'angle se nomme {@html math('\\widehat{\\text{BAC}}')} ou {@html math('\\widehat{\\text{CAB}}')}.
-			</Item>
-			<Item>
-				Le sommet de l'angle est {@html math('\\text{A}')}.
-			</Item>
-			<Item>
-				Les côtés de l'angle sont {@html math('[\\text{AB})')} et {@html math('[\\text{AC})')}.
-			</Item>
+			<Angle r={100} points={[C, A, B]} fill="red" />
+			<Point point={A} nom="A" type={{ forme: 'disque', taille: 10 }} dx={-50} dy={0} />
+			<Point point={B} nom="B" type={{ forme: 'croix', taille: 15 }} dx={-50} dy={0} />
+			<Point point={C} nom="C" type={{ forme: 'croix', taille: 15 }} dy={-50} />
+			<DemiDroite origine={A} passantPar={B} stroke-width={5} />
+			<DemiDroite origine={A} passantPar={C} stroke-width={5} />
 		{/snippet}
 	</Schema>
+	<Texte couleur="noir" lignes={3}>
+		<Item>
+			L'angle se nomme {@html math('\\widehat{\\text{BAC}}')} ou {@html math('\\widehat{\\text{CAB}}')}.
+		</Item>
+		<Item>
+			Le sommet de l'angle est {@html math('\\text{A}')}.
+		</Item>
+		<Item>
+			Les côtés de l'angle sont {@html math('[\\text{AB})')} et {@html math('[\\text{AC})')}.
+		</Item>
+	</Texte>
 	<Notation>
 		Un angle se nomme avec trois lettres, le sommet est la <i>lettre du milieu</i>.
 	</Notation>
