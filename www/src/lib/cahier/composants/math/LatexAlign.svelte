fcb9d230b9c7309715c9905a7d8d0288862ad96f
Valentin Mastro <valentinmastro@hotmail.fr>
2025-11-01 11:46:17 +0100

    LatexAlign extends HTMLDivElement

--- www/src/lib/cahier/composants/math/LatexAlign.svelte
+++ www/src/lib/cahier/composants/math/LatexAlign.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
 	import { alignStar } from 'mathlifier';
 	import LigneVide from '$lib/cahier/composants/LigneVide.svelte';
 
-	type Props = {
+	type Props = HTMLAttributes<HTMLDivElement> & {
 		code: string;
 		lignes?: number;
 		lignes_vides?: number;
 	};
 
-	let { code, lignes = 1, lignes_vides = 0 }: Props = $props();
+	let { code, lignes = 1, lignes_vides = 0, ...props }: Props = $props();
+	let style = (props.style ?? '') + `--lignes: ${lignes};`;
 </script>
 
-<div class="align" style="--lignes: {lignes};">
+<div class="align" {...props} {style}>
 	{@html alignStar(code)}
 </div>
 {#if lignes_vides > 0}
@@ -24,6 +26,7 @@
 		height: calc(var(--lignes) * var(--carreau));
 		overflow-y: hidden;
 		font-size: var(--font-size);
+		line-height: var(--line-height);
 	}
 	:global(.align mtd.tml-right),
 	:global(.align mtd.tml-left) {
