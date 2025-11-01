f9c007813c0dc534952807b787a661c283d93872
Valentin Mastro <valentinmastro@hotmail.fr>
2025-11-01 11:47:25 +0100

    Màj : Convertir les unités

--- www/src/lib/cahier/cours/4eme/07 - Puissances/Page6.svelte
+++ www/src/lib/cahier/cours/4eme/07 - Puissances/Page6.svelte
@@ -2,7 +2,6 @@
 	import { Contenu, SousPartie } from '$lib/cahier/composants/de_chapitrage/*';
 	import { Methode, Exemple } from '$lib/cahier/composants/de_cours/*';
 	import { LatexAlign } from '$lib/cahier/composants/math/*';
-
 	import { math } from 'mathlifier';
 </script>
 
@@ -10,26 +9,29 @@
 	<SousPartie numero={5} titre="Conversion d'unités" />
 	<Methode>Comment convertir une valeur dans une autre unité ?</Methode>
 	<Exemple>
-		Convertir {@html math('9 \\times 10^{11}')} mW en GW <small>(Puissance électrique d'un réacteur nucléaire)</small>
+		Convertir {@html math('900~000~000')} W en GW <small>(Puissance électrique d'un réacteur nucléaire)</small>
 	</Exemple>
 
 	<LatexAlign
-		code={` 9 \\times 10^{11} \\color{red} \\text{ mW} \\color{blue}
-                &= 9 \\times 10^{11} \\color{red}\\times 10^{-12} \\text{ GW} \\color{blue} \\\\
-                &= 9 \\times 10^{11+(-12)} \\text{ GW} \\\\
-                &= 9 \\times 10^{-1} \\text{ GW} `}
-		lignes={4}
+		code={` \\textcolor{forestgreen}{900~000~000} \\text{ W}
+                &= \\color{forestgreen} 9 \\times 10^8 \\color{red} \\text{ W} \\\\
+                &= 9 \\times 10^8 \\color{red} \\times 10^{-9} \\text{ GW} \\\\
+                &= 9 \\times 10^{8+(-9)} \\text{ GW} \\\\
+                &= 9 \\times 10^{-1} \\text{ GW} \\\\
+                &= 0{,}9 \\text{ GW}
+        `}
+		lignes={5}
 	/>
 
 	<Exemple>
-		Convertir 0,000 000 84 nm en km <small>(Rayon d'un proton)</small>
+		Convertir 0,000 000 84 nm en m <small>(Rayon d'un proton)</small>
 	</Exemple>
 	<LatexAlign
 		code={` 0{,}000~000~84 \\color{red} \\text{ nm} \\color{blue}
                 &= \\color{forestgreen} 8{,}4 \\times 10^{-7} \\color{blue} \\color{red} \\text{ nm} \\color{blue} \\\\
-                &= 8{,}4 \\times 10^{-7} \\color{red} \\times 10^{-12} \\text{ km} \\color{blue} \\\\
-                &= 8{,}4 \\times 10^{-7+(-12)} \\text{ km} \\\\
-                &= 8{,}4 \\times 10^{-19} \\text{ km} `}
+                &= 8{,}4 \\times 10^{-7} \\color{red} \\times 10^{-9} \\text{ m} \\color{blue} \\\\
+                &= 8{,}4 \\times 10^{-7+(-9)} \\text{ m} \\\\
+                &= 8{,}4 \\times 10^{-16} \\text{ m} `}
 		lignes={5}
 	/>
 </Contenu>
