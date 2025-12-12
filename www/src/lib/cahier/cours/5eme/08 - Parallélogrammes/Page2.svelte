3f62cc19ff87b2e18937736cd66aca374f4863e5
Valentin Mastro <valentinmastro@hotmail.fr>
2025-12-12 17:57:19 +0100

    Ajout de OnSaitQue, Or, Donc + réécriture du chapitre sur les parallélogrammes

--- www/src/lib/cahier/cours/5eme/08 - Parallélogrammes/Page2.svelte
+++ www/src/lib/cahier/cours/5eme/08 - Parallélogrammes/Page2.svelte
@@ -1,22 +1,44 @@
 <script lang="ts">
 	import { Contenu, Partie } from '$lib/cahier/composants/de_chapitrage/*';
-	import { Definition, Demonstration, Propriete } from '$lib/cahier/composants/de_cours/*';
-	import Texte from '$lib/cahier/composants/de_cours/Texte.svelte';
-	import LigneVide from '$lib/cahier/composants/LigneVide.svelte';
+	import { Definition, Demonstration, Donc, Item, OnSaitQue, Or, Propriete, Schema } from '$lib/cahier/composants/de_cours/*';
+	import { Polygone, Segment, type Coordonnées2D } from '$lib/cahier/composants/svg/*';
+	import { math } from 'mathlifier';
+
+	let points: Coordonnées2D[] = [
+		{ x: 200, y: 200 },
+		{ x: 1800, y: 200 },
+		{ x: 1800, y: 800 },
+		{ x: 200, y: 800 }
+	];
 </script>
 
 <Contenu>
 	<Partie numero={2} titre="Les différents types de parallélogrammes" />
 	<Definition>Un rectangle est un parallélogramme qui a un angle droit.</Definition>
-	<Demonstration lignes={4}>
-		Si un parallélogramme a un angle droit, alors l'angle opposé est aussi un angle droit.<br />
-		De plus, deux angles consécutifs sont supplémentaires, donc les deux autres angles sont aussi droits.
-	</Demonstration>
+	<Demonstration />
+	<Schema lignes={5} aspectRatioSVG={2}>
+		{#snippet svg()}
+			<Polygone {points} afficher_points />
+		{/snippet}
+	</Schema>
+	<OnSaitQue lignes={2}>
+		<Item>ABCD est un parallélogramme.</Item>
+		<Item>{@html math('\\widehat{\\text{DAB}} = 90^\\circ')}</Item>
+	</OnSaitQue>
+	<Or lignes={3}>
+		Dans un parallélogramme :
+		<Item>les angles opposés sont de la même mesure</Item>
+		<Item>2 angles consécutifs sont supplémentaires (= 180°)</Item>
+	</Or>
+	<Donc>
+		{@html math('\\widehat{\\text{ABC}} = \\widehat{\\text{BCD}} = \\widehat{\\text{CDA}} = \\widehat{\\text{DAB}} = 90^\\circ')}
+	</Donc>
 	<Propriete>Les diagonales d'un rectangle sont de même longueur.</Propriete>
-	<Definition>Un losange est un parallélogramme qui a ses côtés de même longueur.</Definition>
-	<Propriete>Les diagonales d'un losange sont perpendiculaires.</Propriete>
-	<Definition lignes={2}>
-		Un carré est un rectangle et un losange.<br />
-		Il hérite des propriétés du rectangle et du losange.
-	</Definition>
+	<Schema lignes={5} aspectRatioSVG={2}>
+		{#snippet svg()}
+			<Polygone {points} afficher_points />
+			<Segment points={[points[0], points[2]]} stroke="red" />
+			<Segment points={[points[1], points[3]]} stroke="red" />
+		{/snippet}
+	</Schema>
 </Contenu>
---
+++ www/src/lib/cahier/cours/5eme/08 - Parallélogrammes/Page3.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Contenu } from '$lib/cahier/composants/de_chapitrage/*';
+	import { Definition, Propriete, Schema } from '$lib/cahier/composants/de_cours/*';
+	import { Polygone, type Coordonnées2D } from '$lib/cahier/composants/svg/*';
+
+	let losange: Coordonnées2D[] = [
+		{ x: 500, y: 100 },
+		{ x: 650, y: 500 },
+		{ x: 500, y: 900 },
+		{ x: 350, y: 500 }
+	];
+
+	let carré: Coordonnées2D[] = [
+		{ x: 200, y: 200 },
+		{ x: 800, y: 200 },
+		{ x: 800, y: 800 },
+		{ x: 200, y: 800 }
+	];
+</script>
+
+<Contenu>
+	<Definition>Un losange est un parallélogramme qui a ses côtés de même longueur.</Definition>
+	<Propriete>Les diagonales d'un losange sont perpendiculaires.</Propriete>
+	<Schema lignes={5}>
+		{#snippet svg()}
+			<Polygone points={losange} />
+		{/snippet}
+	</Schema>
+	<Definition lignes={2}>
+		Un carré est un rectangle et un losange.<br />
+		Il hérite des propriétés du rectangle et du losange.
+	</Definition>
+	<Schema lignes={8}>
+		{#snippet svg()}
+			<Polygone points={carré} />
+		{/snippet}
+	</Schema>
+</Contenu>
