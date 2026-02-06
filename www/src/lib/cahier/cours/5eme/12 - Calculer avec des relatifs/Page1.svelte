4bd1ab098b88571a1c31c6ccad636551d46bc42c
Valentin Mastro <valentinmastro@gmail.com>
2026-02-06 21:29:02 +0100

    ch12 5ème

---
+++ www/src/lib/cahier/cours/5eme/12 - Calculer avec des relatifs/Page1.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Chapitre, Contenu, Partie } from '$lib/cahier/composants/de_chapitrage/*';
+	import { Definition, Exemples, Item } from '$lib/cahier/composants/de_cours/*';
+	import { math } from 'mathlifier';
+</script>
+
+<Chapitre titre="Calculer avec des relatifs" />
+<Contenu>
+	<Partie numero={1} titre="Valeur absolue" />
+	<Definition>La valeur absolue d'un nombre est le nombre "sans le signe".</Definition>
+	<Exemples lignes={2}>
+		<Item>La valeur absolue de {@html math('+5')} est {@html math('5')}</Item>
+		<Item>La valeur absolue de {@html math('-7')} est {@html math('7')}</Item>
+	</Exemples>
+</Contenu>
---
+++ www/src/lib/cahier/cours/5eme/12 - Calculer avec des relatifs/Page2.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Chapitre, Contenu, Partie } from '$lib/cahier/composants/de_chapitrage/*';
+	import { Definition, Exemples, Item, Methode } from '$lib/cahier/composants/de_cours/*';
+	import { math } from 'mathlifier';
+</script>
+
+<Contenu>
+	<Partie numero={2} titre="Addition" />
+	<Methode lignes={5}>
+		Pour additionner deux nombres relatifs :
+		<Item lignes={2}>s'ils sont de même signe, alors la somme est du même signe, et on additionne les valeurs absolues</Item>
+		<Item lignes={2}>
+			s'ils sont de signes différents, la somme est du signe du terme ayant la plus grande valeur absolue, et on soustrait les valeurs absolues
+		</Item>
+	</Methode>
+	<Exemples lignes={2}>
+		<Item>{@html math('(-7) + (-9) = (-16)')}</Item>
+		<Item>{@html math('(-8) + (+5) = -3')}</Item>
+	</Exemples>
+</Contenu>
