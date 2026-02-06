9dff206c49d0c47d0f79d7066acfa9b2307cf487
Valentin Mastro <valentinmastro@gmail.com>
2026-02-06 21:38:58 +0100

    Correction du parse

--- www/src/routes/cahier_d_exercices/[niveau]/+page.ts
+++ www/src/routes/cahier_d_exercices/[niveau]/+page.ts
@@ -1,15 +1,17 @@
 import type { PageLoad } from './$types';
 
-function parse(niveau: string): number {
+function parse(niveau: string): 3 | 4 | 5 | 6 {
+	let n: number | undefined;
 	if (!Number.isNaN(Number.parseInt(niveau, 10))) {
-		return Number.parseInt(niveau, 10);
+		n = Number.parseInt(niveau, 10);
 	} else {
 		let test = niveau.match(/(\d+)eme/);
-		if (test) {
-			return Number.parseInt(test[1], 10);
+		if (test?.[1]) {
+			n = Number.parseInt(test[1], 10);
 		}
 	}
-	return 0;
+	if (n === 3 || n === 4 || n === 5 || n === 6) return n;
+	return 6;
 }
 
 export const load: PageLoad = ({ params }) => {
